14b753331708ca3ceb4bcbaf9245d3c5c1057c7a
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-04-06 15:54:27 +0300

    Added license headers and dep5 file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<!--
+SPDX-FileCopyrightText: 2023 HH Partners
+
+SPDX-License-Identifier: MIT
+-->
+
 # Double Open Server
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,10 @@
+// SPDX-FileCopyrightText: 2023 HH Partners
+//
+// SPDX-License-Identifier: MIT
+
 module.exports = {
     extends: ["next", "turbo", "prettier"],
     rules: {
-      "@next/next/no-html-link-for-pages": "off",
+        "@next/next/no-html-link-for-pages": "off",
     },
-  };
+};
