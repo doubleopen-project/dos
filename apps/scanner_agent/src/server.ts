117597db26090f73099d50a40c8a2de0744b2457
Jyrki Keisala <jyrki.keisala@gmail.com>
2023-04-14 15:30:43 +0300

    First implementation of nodes /job and /job/:id

--- apps/scanner_agent/src/server.ts
+++ apps/scanner_agent/src/server.ts
@@ -4,12 +4,25 @@
 
 import express from 'express';
 import router from './routes/router';
+import Queue from 'bull';
 
 const app = express();
 
+// Serve on PORT on Heroku and on localhost:5001 locally
+const PORT = process.env.PORT || 5001;
+
+// Connect to a local redis intance locally, and the Heroku-provided URL in production
+let REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
+// Create / Connect to a named work queue
+let workQueue = new Queue('work', REDIS_URL);
+
 app.use('/', router);
 
-const PORT = process.env.PORT || 5001;
+// Listen to global events to get notified when jobs are processed
+workQueue.on('global:completed', (jobId, result) => {
+  console.log(`Job completed with result ${result}`);
+});
 
 app.listen(PORT, () =>
   console.log(`Scanner Agent server listening on port ${PORT}`),
