e9e346d8775cbe885ef5c18a6c5f5effe7cb2a95
Jyrki Keisala <jyrki.keisala@gmail.com>
2023-04-14 15:30:43 +0300

    First implementation of nodes /job and /job/:id

--- apps/scanner_agent/src/routes/router.ts
+++ apps/scanner_agent/src/routes/router.ts
@@ -3,13 +3,48 @@
 // SPDX-License-Identifier: MIT
 
 import express from 'express';
+import Queue from 'bull';
+
+// Connect to Heroku-provided URL on Heroku and local redis instance locally
+const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+
+// Create/connect to a named work queue
+let workQueue = new Queue("scanner", REDIS_URL);
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// Node: Hello World
+router.get("/", (req, res) => {
     res.status(200).json({
         "message": "Hello World from Scanner Agent"
     });
-})
+});
+
+// Node: POST a new scanner job. This is yet empty (dummy job)
+router.post("/job", async (req, res) => {
+    const job = await workQueue.add({});
+    
+    res.status(201).json({
+        id: job.id,
+        name: job.name
+    });
+});
+
+// Node: Query job details for job [id]
+router.get("/job/:id", async(req, res) => {
+    let id = req.params.id;
+    let job = await workQueue.getJob(id);
+
+    if (job === null) {
+        res.status(404).json({
+            "message": "No such job in the work queue!"
+        }).end();
+    } else {
+        let state = await job.getState();
+        let progress = job.progress;
+        let reason = job.failedReason;
+        res.status(200).json({id, state, progress, reason});
+    }
+});
 
 export default router;
---
+++ apps/scanner_agent/src/worker.ts
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: 2023 HH Partners
+//
+// SPDX-License-Identifier: MIT
+
+/* This is a first reference implementation of a Bull messaging queue worker.
+   Eventually this will be the one running ScanCode background processes.
+*/
+
+import Queue from "bull";
+import throng from "throng";
+
+// Connect to Heroku-provided URL on Heroku and local redis instance locally
+const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+
+// How many workers for ScanCode
+const WORKERS = process.env.WEB_CONCURRENCY || 2;
+
+// The maximum number of jobs each worker should process at once. This will need
+// to be tuned for your application. If each job is mostly waiting on network 
+// responses it can be much higher. If each job is CPU-intensive, it might need
+// to be much lower.
+let maxJobsPerWorker: number = 10;
+
+const sleep = async (ms: number) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const start = () => {
+    
+    // Connect to the named work queue
+    let workQueue = new Queue("scanner", REDIS_URL);
+
+    workQueue.process(maxJobsPerWorker, async (job) => {
+
+        // A dummy example job for the worker
+        let progress: number = 0;
+        while (progress < 100) {
+            sleep(50);
+            progress++;
+            job.progress(progress);
+        }
+        return {
+            value: "This will be stored."
+        }
+    });
+
+}
+
+// Initialise the clustered worker process
+throng({
+    workers: WORKERS, start
+});
