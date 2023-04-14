314252a2dd8fae10359c9b8f38fa5ed73f03838b
Jyrki Keisala <jyrki.keisala@gmail.com>
2023-04-14 10:21:48 +0300

    Added basic Hello World server

--- apps/scanner_agent/src/server.ts
+++ apps/scanner_agent/src/server.ts
@@ -2,14 +2,14 @@
 //
 // SPDX-License-Identifier: MIT
 
-import express, { Application } from 'express';
+import express from 'express';
 import router from './routes/router';
 
-const app: Application = express();
+const app = express();
 
 app.use('/', router);
 
-const PORT: number = process.env.PORT? parseInt(process.env.PORT) : 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () =>
   console.log(`Scanner Agent server listening on port ${PORT}`),
