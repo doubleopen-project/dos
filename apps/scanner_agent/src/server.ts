bace15065a6b60945d24932347f84219c09ef6f3
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-04-17 12:26:05 +0300

    Added type declarations

--- apps/scanner_agent/src/server.ts
+++ apps/scanner_agent/src/server.ts
@@ -2,14 +2,14 @@
 //
 // SPDX-License-Identifier: MIT
 
-import express from 'express';
+import express, { Application } from 'express';
 import router from './routes/router';
 
-const app = express();
+const app: Application = express();
 
 app.use('/', router);
 
-const PORT = process.env.PORT || 5001;
+const PORT: number = process.env.PORT? parseInt(process.env.PORT) : 5001;
 
 app.listen(PORT, () =>
   console.log(`Scanner Agent server listening on port ${PORT}`),
