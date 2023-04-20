d2feb2a4de0c3bfc52d652127e973f4a8d998f32
Jyrki Keisala <jyrki.keisala@gmail.com>
2023-04-20 15:42:40 +0300

    scanner_agent lint cleanup and worker dummy impl,

--- apps/scanner_agent/src/routes/router.ts
+++ apps/scanner_agent/src/routes/router.ts
@@ -2,27 +2,32 @@
 //
 // SPDX-License-Identifier: MIT
 
-import express from 'express';
+/*
+eslint-disable @typescript-eslint/no-misused-promises
+*/
+
+import express, { Request, Response, Router } from 'express';
 import Queue from 'bull';
 
 // Connect to Heroku-provided URL on Heroku and local redis instance locally
-const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+const REDIS_URL: string = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 
 // Create/connect to a named work queue
-let workQueue = new Queue("scanner", REDIS_URL);
+const workQueue: Queue.Queue = new Queue("scanner", REDIS_URL);
 
-const router = express.Router();
+const router: Router = express.Router();
 
 // Node: Hello World
-router.get("/", (req, res) => {
+router.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         "Message": "Hello World from Scanner Agent"
     });
+    return;
 });
 
 // Node: POST a new scanner job. This is yet empty (dummy job)
-router.post("/job", async (req, res) => {
-    const job = await workQueue.add({});
+router.post("/job", async (req: Request, res: Response) => {
+    const job: Queue.Job = await workQueue.add({});
     
     res.status(201).json({
         id: job.id,
@@ -31,20 +36,61 @@ router.post("/job", async (req, res) => {
 });
 
 // Node: Query job details for job [id]
-router.get("/job/:id", async(req, res) => {
-    let id = req.params.id;
-    let job = await workQueue.getJob(id);
+router.get("/job/:id", async(req: Request, res: Response) => {
+    const id: string = req.params.id;
+    const job: Queue.Job | null = await workQueue.getJob(id);
 
     if (job === null) {
         res.status(404).json({
-            "Message": "No such job in the work queue!"
-        }).end();
+            "Message": "No such job in the work queue"
+        });
     } else {
-        let state = await job.getState();
-        let progress = job.progress;
-        let reason = job.failedReason;
-        res.status(200).json({id, state, progress, reason});
+        const state: string = await job.getState();
+        const progress: number = job.progress();
+        const finishedOn: number | undefined = job.finishedOn;
+        res.status(200).json({
+            id, 
+            state, 
+            progress, 
+            finishedOn
+        });
     }
 });
+
+// Trying to write the same functions using a different structure to make Lint happy
+
+router.post("/jobtemp", function(req: Request, res: Response) {
+
+    workQueue.add({})
+        .then(function(jobResolved: Queue.Job) {
+            const job: Queue.Job = jobResolved;
+            return res.status(201).json({
+                id: job.id,
+                name: job.name
+            })
+        })
+        .catch(function(err: string) {
+            console.log("Failed to push a job to the queue, reason", err);
+            return res.status(500).json({"Message": "Internal server error: Bull"});
+        }) 
+});
+
+router.get("/jobtemp/:id", function(req: Request, res: Response) {
+    const id: string = req.params.id;
+    
+    workQueue.getJob(id)
+        .then(function(jobResolved: Queue.Job | null) {
+            if (jobResolved) {
+                // TODO
+            }
+        })
+        .catch(function() {
+            return res.status(404).json({
+                "Message": "No such job in the work queue!"
+            });
+        })
+});
+
+
 
 export default router;
