97932ed9509cac6c278696098026b4e0689e3740
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-04-28 16:05:29 +0300

    Added database package with Prisma setup, moved schema

---
+++ packages/database/src/client.ts
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2023 HH Partners
+//
+// SPDX-License-Identifier: MIT
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export * from "@prisma/client";
