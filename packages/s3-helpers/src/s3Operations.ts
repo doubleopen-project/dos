92e09752864dfc3f993272ffc093c23b8730b506
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-05-05 14:34:46 +0300

    Refactored S3 operations

--- packages/s3-helpers/src/s3Operations.ts
+++ packages/s3-helpers/src/s3Operations.ts
@@ -4,15 +4,18 @@
 
 // All needed file operations for Digital Ocean Spaces
 
-import { ListBucketsCommand, 
-    ListObjectsCommand, 
-    ListObjectsCommandInput, 
-    PutObjectCommand, 
+import {
+    ListBucketsCommand,
+    ListObjectsCommand,
+    ListObjectsCommandInput,
+    PutObjectCommand,
     PutObjectCommandInput,
-    GetObjectCommand } from '@aws-sdk/client-s3';
-import { s3Client } from '../models/s3Client';
+    GetObjectCommand
+} from '@aws-sdk/client-s3';
+import { s3Client } from './s3Client';
 import * as fs from 'fs';
 import * as path from 'path';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 // List all buckets in the account
 export const listBuckets = async (): Promise<string | undefined> => {
@@ -33,7 +36,7 @@ export const listObjects = async (bucketName: string): Promise<string | undefine
     } catch (err) {
         console.log("Error trying to return a list of S3 bucket objects", err);
     }
-}; 
+};
 
 // Upload a file to a bucket
 export const uploadFile = async (bucketName: string, fileName: string, fileContent: string): Promise<string | undefined> => {
@@ -49,7 +52,7 @@ export const uploadFile = async (bucketName: string, fileName: string, fileConte
 
 // Download an entire directory of files from a bucket to a local directory
 export const downloadDirectory = async (bucketName: string, dirS3: string, baseDir: string): Promise<string> => {
-    
+
     // Don't try to retrieve from an empty directory
     if (await isDirectoryEmpty(bucketName, dirS3)) {
         console.log("Error: trying to download an empty or non-existing directory from S3");
@@ -73,7 +76,7 @@ export const downloadDirectory = async (bucketName: string, dirS3: string, baseD
     await Promise.all(files.map(async (file) => {
         const filePath = path.join(baseDir, file.Key || "");
         const dirPath = path.dirname(filePath);
-        
+
         // Create the local directory if it does not exist
         if (!fs.existsSync(dirPath)) {
             fs.mkdirSync(dirPath, { recursive: true });
@@ -120,5 +123,43 @@ const isDirectoryEmpty = async (bucketName: string, directoryName: string): Prom
         return false;
     } else {
         return true;
+    }
+}
+
+// Get presigned put url
+export const getPresignedPutUrl = async (key: string): Promise<string | undefined> => {
+    try {
+        //TODO: Check that the object doesn't already exist
+        checkS3ClientEnvs();
+
+        if (!process.env.SPACES_BUCKET) {
+            throw new Error("SPACES_BUCKET environment variable is missing");
+        }
+
+        const bucket: string = process.env.SPACES_BUCKET;
+
+        const command: PutObjectCommand = new PutObjectCommand({
+            Bucket: bucket,
+            Key: key,
+        });
+
+        const url: string = await getSignedUrl(s3Client, command);
+        return url;
+
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
+};
+
+const checkS3ClientEnvs = (): void => {
+    if (!process.env.SPACES_ENDPOINT) {
+        throw new Error("SPACES_ENDPOINT environment variable is missing");
+    }
+    if (!process.env.SPACES_KEY) {
+        throw new Error("SPACES_KEY environment variable is missing");
+    }
+    if (!process.env.SPACES_SECRET) {
+        throw new Error("SPACES_SECRET environment variable is missing");
     }
 }
