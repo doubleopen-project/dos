4510a64cd0038136f6320cdeb4c92f2e9877ee41
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-05-30 15:24:34 +0300

    Increased json size limit

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,8 +4,15 @@
 
 import { zodiosApp } from "@zodios/express";
 import router from './routes/router';
+import express from "express";
+import { dosApi } from "validation-helpers";
 
-const app = zodiosApp();
+const opts = {
+    enableJsonBodyParser : false
+}
+const app = zodiosApp(dosApi, opts);
+
+app.use(express.json({limit: '50mb'}));
 
 app.use('/api', router);
 
