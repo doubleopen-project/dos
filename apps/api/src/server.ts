f5fe5c3f73044491193abfe146fcfa93f0895ed8
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-05-30 16:08:13 +0300

    Added building API documentation automatically from zodios api definition

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -6,6 +6,8 @@ import { zodiosApp } from "@zodios/express";
 import router from './routes/router';
 import express from "express";
 import { dosApi } from "validation-helpers";
+import { serve, setup } from 'swagger-ui-express';
+import { openApiBuilder } from "@zodios/openapi";
 
 const opts = {
     enableJsonBodyParser : false
@@ -15,6 +17,21 @@ const app = zodiosApp(dosApi, opts);
 app.use(express.json({limit: '50mb'}));
 
 app.use('/api', router);
+
+const document = openApiBuilder({
+  title: "DOS API",
+  version: "1.0.0",
+  description: "API reference for Double Open Server",
+})
+  // you can declare as many security servers as you want
+  .addServer({ url: "/api" })
+  // you can declare as many apis as you want
+  .addPublicApi(dosApi)
+  .build();
+
+app.use(`/docs/swagger.json`, (_, res) => res.json(document));
+app.use("/docs", serve);
+app.use("/docs", setup(undefined, { swaggerUrl: "/docs/swagger.json" }));
 
 const PORT = process.env.PORT? parseInt(process.env.PORT) : 5000;
 
