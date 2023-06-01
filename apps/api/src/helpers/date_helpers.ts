ea23a80e078026a5d0dac4473fb2d991cb6ba05c
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-06-01 16:07:39 +0300

    Added formatDateString function and a test for it

---
+++ apps/api/src/helpers/date_helpers.ts
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2023 HH Partners
+//
+// SPDX-License-Identifier: MIT
+
+/*
+This function is needed to reformat start_timestamp and end_timestamp strings 
+from scancode result json so that a Date object can be created from it
+*/
+export const formatDateString = (datestring: string) => {
+    return datestring.slice(0,13) + ':' + datestring.slice(13,15) + ':' + datestring.slice(15);
+}
