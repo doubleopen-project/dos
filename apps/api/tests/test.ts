1586103b3aa46d1643ffaf8c11f8c32b41f69dc2
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-06-01 16:07:39 +0300

    Added formatDateString function and a test for it

--- apps/api/tests/test.ts
+++ apps/api/tests/test.ts
@@ -3,7 +3,9 @@
 // SPDX-License-Identifier: MIT
 
 import routerTestSuite from './suites/router.spec';
+import dateHelpersTestSuite from './suites/date_helpers.spec';
 
-describe('Subject', function () {
+describe('Running API tests', function () {
     describe('Testing router', routerTestSuite.bind(this));
+    describe('Testing date_helpers', dateHelpersTestSuite.bind(this));
 });
