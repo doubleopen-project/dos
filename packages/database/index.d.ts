1df757517f5caf1fad57c59445ce35295017ac98
Jyrki Keisala <jyrki.keisala@gmail.com>
2023-06-08 11:10:14 +0300

    Router "/" endpoint tested, test environment fixed

--- packages/database/index.d.ts
+++ packages/database/index.d.ts
@@ -2,13 +2,12 @@
 //
 // SPDX-License-Identifier: MIT
 
-import * as _prisma_client from '.prisma/client';
 import { PrismaClient } from '@prisma/client';
 export * from '@prisma/client';
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
-declare const prisma: PrismaClient<_prisma_client.Prisma.PrismaClientOptions, never, _prisma_client.Prisma.RejectOnNotFound | _prisma_client.Prisma.RejectPerOperation | undefined>;
+declare const prisma: any;
 
 export { prisma };
