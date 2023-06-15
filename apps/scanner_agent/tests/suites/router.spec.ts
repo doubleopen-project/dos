4e075f1d638be300ef19eb1c4116f45f1cb6b217
Jyrki Keisala <jyrki.keisala@gmail.com>
2023-06-15 13:40:54 +0300

    SA "/" and "/job" endpoints tested using nock

--- apps/scanner_agent/tests/suites/router.spec.ts
+++ apps/scanner_agent/tests/suites/router.spec.ts
@@ -2,27 +2,72 @@
 //
 // SPDX-License-Identifier: MIT
 
-import chai, { assert, expect } from 'chai';
+import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import nock from 'nock';
 import app from "../../src/server";
 
 chai.use(chaiHttp);
 
 export default function suite(): void {
-   it('1+2 should equal 3', function() {
-     const result: number = 1 + 2;
-     assert.strictEqual(result, 3);
-   });
 
-   it('GET / should return 200', done => {
-      chai
-          .request(app)
-          .get('/')
-          .end((err, res) => {
-              expect(res).to.have.status(200);
-              done();
-          }
-      );
-  });
+    it('GET / should return 200 and respond properly', done => {
+
+        // Define the mock response
+        const mockResponse = { message: "Scanner Agent is up and listening!" };
+
+        // Intercept the HTTP request and respond with the mock response
+        nock('http://localhost:5000')
+            .get("/")
+            .reply(200, mockResponse);
+
+        // Make the HTTP request using chai-http
+        chai
+            .request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.deep.equal(mockResponse);
+                done();
+            });
+    });
+
+    it('POST /job should add a new job to the work queue and respond properly', async () => {
+            
+        // Define the mock request body and response
+
+        const mockRequestBody = {
+            directory: "/home/user/test",
+            opts: {
+                jobId: "1234567890"
+            }
+        };
+
+        const mockResponse = {
+            id: "1234567890",
+            data: {
+                directory: "/home/user/test",
+            }
+        };
+
+        // Intercept the HTTP request and respond with the mock response
+        nock('http://localhost:5000')
+            .post("/job", mockRequestBody)
+            .reply(201, mockResponse);
+
+        // Make the HTTP request using chai-http
+        const res = await chai
+            .request(app)
+            .post('/job')
+            .send({
+                directory: "/home/user/test",
+                opts: {
+                    jobId: "1234567890"
+                }
+            });
+
+        expect(res).to.have.status(201);
+        expect(res.body).to.deep.equal(mockResponse);
+    });
 
 }
