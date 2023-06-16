f47d1ff3b7d64a61b15f87777e70cd61fc3975c2
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-06-16 12:20:11 +0300

    Downgraded formidable and typescript packages to fix issues with api tests

--- apps/api/tests/test.ts
+++ apps/api/tests/test.ts
@@ -4,11 +4,6 @@
 
 import routerTestSuite from './suites/router.spec';
 import dateHelpersTestSuite from './suites/date_helpers.spec';
-//import { loadEnv } from 'common-helpers';
-
-//loadEnv('../../../.env');
-console.log(process.env.SPACES_ENDPOINT);
-
 
 describe('Running API tests', function () {
     describe('Testing router', routerTestSuite.bind(this));
