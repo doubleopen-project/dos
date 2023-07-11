6e9139512ba829f4b3182c3e265360ac63e788cd
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-07-11 17:15:16 +0300

    Fixed a bug, and edited console logs and comments

--- apps/api/src/helpers/file_helpers.ts
+++ apps/api/src/helpers/file_helpers.ts
@@ -8,19 +8,16 @@ import path from 'path';
 // Iterate through all files in a directory and return an array of file paths
 
 export const getFilePaths = async (baseDir: string): Promise<string[]> => {
-    // Get the files as an array
+    // Get the file paths as an array
     const filePaths: string[] = [];
     const directories: string[] = [baseDir];
 
     while (directories.length > 0) {
         const currentDir = directories.pop() as string;
-        console.log("currentDir: ", currentDir);
-        
 
         const files = await fs.promises.readdir(currentDir);
-        // Loop them all with the new for...of
-        for (const file of files) {
 
+        for (const file of files) {
             // Get the full paths
             const fromPath = path.join(currentDir, file);
 
@@ -28,11 +25,9 @@ export const getFilePaths = async (baseDir: string): Promise<string[]> => {
             const stat = await fs.promises.stat(fromPath);
 
             if (stat.isFile()) {
-                //console.log("'%s' is a file.", fromPath);
                 const filePath = fromPath.split('/tmp/extracted/').pop();
                 filePaths.push(filePath as string);
             } else if (stat.isDirectory()) {
-                //console.log("'%s' is a directory.", fromPath);
                 directories.push(fromPath);
             }
         }
