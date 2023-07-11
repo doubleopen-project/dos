bf072e566547234881b81b12aa2bb1e4bf3a4cd0
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-07-11 13:52:46 +0300

    Added saving extracted files to object storage

--- apps/api/src/helpers/file_helpers.ts
+++ apps/api/src/helpers/file_helpers.ts
@@ -7,16 +7,20 @@ import path from 'path';
 
 // Iterate through all files in a directory and return an array of file paths
 
-export const getFilePaths = async (dir: string): Promise<string[]> => {
+export const getFilePaths = async (baseDir: string): Promise<string[]> => {
     // Get the files as an array
     const filePaths: string[] = [];
-    const directories: string[] = [dir];
+    const directories: string[] = [baseDir];
 
     while (directories.length > 0) {
         const currentDir = directories.pop() as string;
+        console.log("currentDir: ", currentDir);
+        
+
         const files = await fs.promises.readdir(currentDir);
         // Loop them all with the new for...of
         for (const file of files) {
+
             // Get the full paths
             const fromPath = path.join(currentDir, file);
 
@@ -24,10 +28,11 @@ export const getFilePaths = async (dir: string): Promise<string[]> => {
             const stat = await fs.promises.stat(fromPath);
 
             if (stat.isFile()) {
-                console.log("'%s' is a file.", fromPath);
-                filePaths.push(fromPath);
+                //console.log("'%s' is a file.", fromPath);
+                const filePath = fromPath.split('/tmp/extracted/').pop();
+                filePaths.push(filePath as string);
             } else if (stat.isDirectory()) {
-                console.log("'%s' is a directory.", fromPath);
+                //console.log("'%s' is a directory.", fromPath);
                 directories.push(fromPath);
             }
         }
--- packages/s3-helpers/index.d.ts
+++ packages/s3-helpers/index.d.ts
@@ -6,12 +6,13 @@ import { S3 } from '@aws-sdk/client-s3';
 
 declare const listBuckets: () => Promise<string | undefined>;
 declare const listObjects: (bucketName: string) => Promise<string | undefined>;
-declare const uploadFile: (bucketName: string, fileName: string, fileContent: string) => Promise<string | undefined>;
 declare const downloadFile: (bucketName: string, fileName: string, filePath: string) => Promise<boolean>;
 declare const downloadDirectory: (bucketName: string, dirS3: string, baseDir: string) => Promise<string>;
 declare const objectExistsCheck: (key: string) => Promise<boolean | undefined>;
 declare const getPresignedPutUrl: (key: string) => Promise<string | undefined>;
+declare const uploadFile: (bucketName: string, fileName: string, fileContent: string | Buffer) => Promise<string | undefined>;
+declare const saveFiles: (filePaths: string[], baseDir: string) => void;
 
 declare const client: S3;
 
-export { client, downloadDirectory, downloadFile, getPresignedPutUrl, listBuckets, listObjects, objectExistsCheck, uploadFile };
+export { client, downloadDirectory, downloadFile, getPresignedPutUrl, listBuckets, listObjects, objectExistsCheck, saveFiles, uploadFile };
