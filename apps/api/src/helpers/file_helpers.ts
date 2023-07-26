c3ea90ff9afddb3680d2e88a55c6fc5675a295a3
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-07-26 12:54:48 +0300

    Added skipping .gitignore file

--- apps/api/src/helpers/file_helpers.ts
+++ apps/api/src/helpers/file_helpers.ts
@@ -36,43 +36,6 @@ export const deleteLocalFiles = async (downloadPath: string, extractPath: string
     fs.rmSync(downloadPath);
 }
 
-// Iterate through all files in a directory and return an array of file paths
-export const getFilePaths = async (baseDir: string): Promise<string[]> => {
-    // Get the file paths as an array
-    const filePaths: string[] = [];
-    const directories: string[] = [baseDir];
-
-    while (directories.length > 0) {
-        const currentDir = directories.pop() as string;
-
-        const curDirNoBase = currentDir.split(baseDir).pop();
-        
-        if (curDirNoBase === '.git') {
-            console.log('Skipping .git directory');
-            continue;
-        }
-
-        const files = await fs.promises.readdir(currentDir);
-
-        for (const file of files) {
-            // Get the full paths
-            const fromPath = path.join(currentDir, file);
-
-            // Stat the file to see if we have a file or dir
-            const stat = await fs.promises.stat(fromPath);
-
-            if (stat.isFile()) {
-                const filePath = fromPath.split('/tmp/extracted/').pop();
-                filePaths.push(filePath as string);
-            } else if (stat.isDirectory()) {
-                directories.push(fromPath);
-            }
-        }
-    }
-
-    return filePaths;
-}
-
 // Iterate through all files in a directory and return an array of file hashes and paths 
 export const getFileHashesMappedToPaths = async (baseDir: string): Promise<Array<{ hash: string, path: string }>> => {
     // Get the file paths as an array
@@ -94,6 +57,12 @@ export const getFileHashesMappedToPaths = async (baseDir: string): Promise<Array
         const files = await fs.promises.readdir(currentDir);
 
         for (const file of files) {
+            console.log('File: ' + file);
+            if (file === '.gitignore') {
+                console.log('Skipping .gitignore file');
+                continue;
+            }
+            
             // Get the full paths
             const fromPath = path.join(currentDir, file);
             console.log('fromPath: ' + fromPath);
