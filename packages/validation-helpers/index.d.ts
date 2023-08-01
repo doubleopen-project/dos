50deca621d50b5f2a35c43dfc1e880981accc6cc
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-08-01 09:33:19 +0300

    Removed results from job-state response

--- packages/validation-helpers/index.d.ts
+++ packages/validation-helpers/index.d.ts
@@ -3401,155 +3401,16 @@ declare const dosApi: [{
             message: string;
             status: string;
         }>;
-        results: zod.ZodUnion<[zod.ZodNull, zod.ZodObject<{
-            licenses: zod.ZodArray<zod.ZodObject<{
-                license: zod.ZodString;
-                location: zod.ZodObject<{
-                    path: zod.ZodString;
-                    start_line: zod.ZodNumber;
-                    end_line: zod.ZodNumber;
-                }, "strip", zod.ZodTypeAny, {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                }, {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                }>;
-                score: zod.ZodNumber;
-            }, "strip", zod.ZodTypeAny, {
-                score: number;
-                license: string;
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-            }, {
-                score: number;
-                license: string;
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-            }>, "many">;
-            copyrights: zod.ZodArray<zod.ZodObject<{
-                statement: zod.ZodString;
-                location: zod.ZodObject<{
-                    path: zod.ZodString;
-                    start_line: zod.ZodNumber;
-                    end_line: zod.ZodNumber;
-                }, "strip", zod.ZodTypeAny, {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                }, {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                }>;
-            }, "strip", zod.ZodTypeAny, {
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-                statement: string;
-            }, {
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-                statement: string;
-            }>, "many">;
-        }, "strip", zod.ZodTypeAny, {
-            copyrights: {
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-                statement: string;
-            }[];
-            licenses: {
-                score: number;
-                license: string;
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-            }[];
-        }, {
-            copyrights: {
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-                statement: string;
-            }[];
-            licenses: {
-                score: number;
-                license: string;
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-            }[];
-        }>]>;
     }, "strip", zod.ZodTypeAny, {
         state: {
             message: string;
             status: string;
         };
-        results: {
-            copyrights: {
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-                statement: string;
-            }[];
-            licenses: {
-                score: number;
-                license: string;
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-            }[];
-        } | null;
     }, {
         state: {
             message: string;
             status: string;
         };
-        results: {
-            copyrights: {
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-                statement: string;
-            }[];
-            licenses: {
-                score: number;
-                license: string;
-                location: {
-                    path: string;
-                    start_line: number;
-                    end_line: number;
-                };
-            }[];
-        } | null;
     }>;
     errors: [{
         status: 500;
