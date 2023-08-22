2278122a479de7c13fe26a6883924c8ed3f1fa1d
Etsija <jyrki.keisala@gmail.com>
2023-08-22 10:26:00 +0300

    Add unit tests for compression and decompression

---
+++ packages/common-helpers/tests/test.ts
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2023 HH Partners
+//
+// SPDX-License-Identifier: MIT
+
+import compressionHelperTestSuite from './suites/compressionHelper.spec';
+
+describe('Subject', function () {
+    describe('common-helpers/compressionHelper', compressionHelperTestSuite.bind(this));
+});
