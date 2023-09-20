42021ec6c004d87f0b832127af9527bfbcf594b3
Etsija <jyrki.keisala@gmail.com>
2023-09-20 16:04:41 +0300

    Add filetree and code inspector frames

--- apps/curation_front_end/src/pages/index.tsx
+++ apps/curation_front_end/src/pages/index.tsx
@@ -2,18 +2,19 @@
 //
 // SPDX-License-Identifier: MIT
 
-import Header from '../components/Header'
+import Header from '../components/Header';
+import PackageTree from '../components/PackageTree';
+import CodeInspector from '../components/CodeInspector';
 
 export default function Home() {
     return (
-        <main className='
-            bg-gradient-to-br 
-            from-black 
-            to-gray-900 
-            text-white 
-            min-h-screen'>
+        <main className='bg-gradient-to-br from-black to-gray-900 text-white min-h-screen'>
 
             <Header />
+            <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
+                <PackageTree />
+                <CodeInspector />
+            </div>
         </main>
     )
 }
