a3c381b5a4af2b114a1414410e1f1a8ff6f2710b
Etsija <jyrki.keisala@gmail.com>
2023-09-20 15:09:55 +0300

    Add sidebar

---
+++ apps/curation_front_end/src/components/Sidebar.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Link from 'next/link'
+import { GrInspect, GrCatalog } from 'react-icons/gr'
+import { AiOutlineHome } from 'react-icons/ai'
+
+interface SidebarProps {
+    children: React.ReactNode
+}
+
+const Sidebar = ({ children }: SidebarProps) => {
+    const toolbarSettings = "fixed w-20 h-screen p-4 bg-black border-r-[1px] flex flex-col justify-between";
+    const iconSettings = "bg-gray-600 text-white my-4 p-3 hover:bg-gray-400 rounded-lg inline-block";
+    const negCorrection = "brightness(0) invert(1) sepia(1) saturate(5) hue-rotate(175deg)";
+    return (
+        <div className='flex'>
+            <div className={toolbarSettings}>
+                <div className='flex flex-col items-center'>
+                    <Link href='/'>
+                        <div className={iconSettings} title='Home'>
+                            <AiOutlineHome size={20} />
+                        </div>
+                    </Link>
+                    <Link href='/'>
+                        <div className={iconSettings} title='Inspect & Curate Packages'>
+                            <GrInspect size={20} style={{filter: negCorrection}} />
+                        </div>
+                    </Link>
+                    <Link href='/'>
+                        <div className={iconSettings} title='Curations Library'>
+                            <GrCatalog size={20} style={{filter: negCorrection}} />
+                        </div>
+                    </Link>
+                </div>
+            </div>
+            <main className='ml-20 w-full'>{children}</main>
+        </div>
+    )
+}
+
+export default Sidebar;
--- apps/curation_front_end/src/pages/index.tsx
+++ apps/curation_front_end/src/pages/index.tsx
@@ -5,9 +5,15 @@
 import Header from '../components/Header'
 
 export default function Home() {
-  return (
-    <main className={`bg-gray-100 min-h-screen`}>
-        <Header />
-    </main>
-  )
+    return (
+        <main className='
+            bg-gradient-to-br 
+            from-black 
+            to-gray-900 
+            text-white 
+            min-h-screen'>
+
+            <Header />
+        </main>
+    )
 }
