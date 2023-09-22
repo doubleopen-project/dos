0b1ab03c1a9c66ce82b3ff0efe2bcb3e93958dd8
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-09-22 12:15:36 +0300

    Fix purl for packages that start with @ character

--- apps/curation_front_end/src/pages/packages/[purl].tsx
+++ apps/curation_front_end/src/pages/packages/[purl].tsx
@@ -10,7 +10,12 @@ import { zodiosHooks } from '../../hooks/zodiosHooks'
 
 export default function Package() {
     const router = useRouter();
-    const { purl } = router.query;
+    let { purl } = router.query;
+
+    // If purl has /@ in a row, the characters should be changed to /%40
+    // This is because in the purl spec, @ is used to separate the package name and the version
+
+    purl = purl?.toString().replace(/\/@/g, '/%40');
 
     const { data, isLoading, error } = zodiosHooks.useImmutableQuery('/filetree', { purl: purl as string });
     if (isLoading) {
