e4e6fc94b37516d121a3d18da59aa6e80629d502
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-09-27 16:56:40 +0300

    Added login form

---
+++ apps/curation_front_end/src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2023 HH Partners
+//
+// SPDX-License-Identifier: MIT
+
+import LoginForm from "@/components/LoginForm";
+
+export default function Login() {
+    return (
+        <main className='bg-gray-200 h-screen flex justify-center items-center'>
+                <LoginForm />
+        </main>
+    )
+}
--- packages/validation-helpers/index.d.ts
+++ packages/validation-helpers/index.d.ts
@@ -3627,4 +3627,16 @@ declare const GetPackagesRes: z.ZodObject<{
 }>;
 type GetPackagesResType = z.infer<typeof GetPackagesRes>;
 
-export { CreateCopyrightFindingInput, CreateFileInput, CreateFileTreeInput, CreateLicenseConclusionInput, CreateLicenseFindingInput, CreateLicenseFindingMatchInput, CreatePackageInput, CreateScanIssueInput, CreateScannerJobInput, DBFileSchema, DBScannerJobSchema, DBScannerJobType, FileTreeType, GetPackagesResType, PostFileTreeResType, ScannerJobOnlyIdOutput, ScannerJobResultSchema, UpdateFileInput, UpdatePackageInput, UpdateScannerJobInput, dosApi, scannerAgentApi };
+declare const loginFormSchema: z.ZodObject<{
+    username: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    username: string;
+    password: string;
+}, {
+    username: string;
+    password: string;
+}>;
+type LoginFormType = z.infer<typeof loginFormSchema>;
+
+export { CreateCopyrightFindingInput, CreateFileInput, CreateFileTreeInput, CreateLicenseConclusionInput, CreateLicenseFindingInput, CreateLicenseFindingMatchInput, CreatePackageInput, CreateScanIssueInput, CreateScannerJobInput, DBFileSchema, DBScannerJobSchema, DBScannerJobType, FileTreeType, GetPackagesResType, LoginFormType, PostFileTreeResType, ScannerJobOnlyIdOutput, ScannerJobResultSchema, UpdateFileInput, UpdatePackageInput, UpdateScannerJobInput, dosApi, loginFormSchema, scannerAgentApi };
