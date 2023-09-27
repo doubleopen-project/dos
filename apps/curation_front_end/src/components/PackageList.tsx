237db2e25fbddc45745878d00ebb471a73e2732a
Etsija <jyrki.keisala@gmail.com>
2023-09-27 13:01:03 +0300

    Fix reuse

--- apps/curation_front_end/src/components/PackageList.tsx
+++ apps/curation_front_end/src/components/PackageList.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 HH Partners
+//
+// SPDX-License-Identifier: MIT
+
 import React, { useState } from 'react'
 import type { GetPackagesResType } from 'validation-helpers';
 
