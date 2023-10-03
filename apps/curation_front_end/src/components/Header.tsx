0edfc22efaf4f034433170ec05b5610f70334135
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-10-03 17:39:03 +0300

    Edited server config and getting user data on front end

--- apps/curation_front_end/src/components/Header.tsx
+++ apps/curation_front_end/src/components/Header.tsx
@@ -4,12 +4,16 @@
 
 import React from 'react';
 import logo from '@/public/icons/Double_Open_logo.png';
+import { useUser } from '@/hooks/useUser';
 
 const Header = () => {
+    const user = useUser({});
+
+    const username = user ? user.username : 'Guest';
     return (
         <div className='flex bg-gray-200 justify-between p-4'>
             <img src={logo.src} alt='DoubleOpen Logo' width='160' />
-            <h2>Welcome back, willebra</h2>
+            <h2>Welcome back, {username}</h2>
         </div>
     );
 };
