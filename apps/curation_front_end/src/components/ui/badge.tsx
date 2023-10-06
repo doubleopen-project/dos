bb77ff70ac25230bffe4d7041b6acce3e3edc13b
Etsija <jyrki.keisala@gmail.com>
2023-10-06 15:17:10 +0300

    Fix reuse

--- apps/curation_front_end/src/components/ui/badge.tsx
+++ apps/curation_front_end/src/components/ui/badge.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 HH Partners
+//
+// SPDX-License-Identifier: MIT
+
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
