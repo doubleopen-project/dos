fe05dd30255693786acb4bb1ed458f1a8fb8dd8d
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-10-09 16:09:55 +0300

    Edited Header and Sidebar rendering and added redirect on successful logout

--- apps/curation_front_end/src/pages/logout/index.tsx
+++ apps/curation_front_end/src/pages/logout/index.tsx
@@ -3,41 +3,44 @@
 // SPDX-License-Identifier: MIT
 
 import { Loader2 } from 'lucide-react';
-import { useQueryClient } from '@tanstack/react-query';
 import { authHooks } from '@/hooks/zodiosHooks';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Logout() {
-    const { data, error, isSuccess, isLoading } = authHooks.useImmutableQuery('/logout', undefined, { withCredentials: true })
-    
-    const queryClient = useQueryClient();
+    const [counter, setCounter] = useState(3);
+    const { error, isSuccess, isLoading } = authHooks.useImmutableQuery('/logout', undefined, { withCredentials: true })
+    const router = useRouter();
 
     useEffect(() => {
+        if (counter === 0) router.push('/');
         if (isSuccess) {
-            queryClient.removeQueries(['user']);
+            setTimeout(() => {
+                setCounter(counter - 1);
+            }, 1000);
         }
-    }, [isSuccess, queryClient])
+    }, [counter, router, isSuccess]);
 
     return (
         <div className='bg-gray-200 h-screen'>
             {isLoading &&
                 <div className='flex justify-center items-center h-full bg-gray-200'>
-                    <Loader2 className='mr-2 h-16 w-16 animate-spin' />
+                    <div className='text-lg w-72 bg-white rounded-md h-min p-10 flex justify-center'>
+                        <Loader2 className='mr-2 h-16 w-16 animate-spin' />
+                    </div>
                 </div>
             }
             {error &&
                 <div className='flex justify-center items-center h-full bg-gray-200'>
-                    <h1 className='text-2xl'>Error</h1>
+                    <h1 className='text-lg w-72 bg-white rounded-md h-min p-10'>Error</h1>
                 </div>
             }
             {isSuccess &&
                 <div className='flex justify-center items-center h-full bg-gray-200'>
-                    <h1 className='text-2xl'>Logged out successfully</h1>
-                </div>
-            }
-            {data && 
-                <div className='flex justify-center items-center h-full bg-gray-200'>
-                    <h1 className='text-2xl'>{data.message}</h1>
+                    <div className='text-lg w-72 bg-white rounded-md h-min p-10'>
+                        <p>Logged out successfully.</p>
+                        <p>Redirecting in {counter} seconds.</p>
+                    </div>
                 </div>
             }
         </div>
