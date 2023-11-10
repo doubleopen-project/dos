0c8120e3640574e3210f7367f3acc603132f0c06
Etsija <jyrki.keisala@gmail.com>
2023-11-10 08:50:10 +0200

    Storing and fetching resized width from zustand

--- apps/curation_front_end/src/components/CurationUI.tsx
+++ apps/curation_front_end/src/components/CurationUI.tsx
@@ -2,9 +2,10 @@
 //
 // SPDX-License-Identifier: MIT
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PackageTree from "@/components/PackageTree";
 import CodeInspector from "@/components/CodeInspector";
+import useSettingsStore from "@/store/settings.store";
 
 type CurationUIProps = {
     purl: string | undefined;
@@ -12,10 +13,47 @@ type CurationUIProps = {
 };
 
 const CurationUI = ({ purl, path }: CurationUIProps) => {
+    const treeWidth = useSettingsStore((state) => state.treeWidth);
+    const setTreeWidth = useSettingsStore((state) => state.setTreeWidth);
+    const treeRef = useRef<HTMLDivElement>(null);
+
+    // Ensure the initial width is set based on the persisted value from Zustand
+    useEffect(() => {
+        if (treeWidth !== null && treeWidth !== undefined) {
+            // Use the persisted treeWidth if available
+            treeRef.current?.style.setProperty(
+                "width",
+                `${treeWidth}px`,
+                "important",
+            );
+        } else {
+            // Set the default width if Zustand value is not available
+            treeRef.current?.style.removeProperty("width");
+        }
+    }, [treeWidth]);
+
+    useEffect(() => {
+        // Event listener for mouseup event on window
+        const handleMouseUp = () => {
+            if (treeRef.current) {
+                setTreeWidth(treeRef.current.offsetWidth);
+            }
+        };
+        // Add the event listener when the component mounts
+        window.addEventListener("mouseup", handleMouseUp);
+        // Cleanup the event listener when the component unmounts
+        return () => {
+            window.removeEventListener("mouseup", handleMouseUp);
+        };
+    }, []);
+
     return (
         <div className="flex flex-col md:flex-row md:h-screen overflow-auto">
             {/* 1st column: Show and filter package */}
-            <div className="flex flex-col m-2 mr-1 p-2 rounded-md border shadow-lg overflow-auto resize-x">
+            <div
+                ref={treeRef}
+                className="flex flex-col m-2 mr-1 p-2 rounded-md border shadow-lg overflow-auto resize-x"
+            >
                 <PackageTree purl={purl} />
             </div>
 
