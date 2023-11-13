26dbe888c85c09451bf1d1fb9e1e7568fcb62d82
Etsija <jyrki.keisala@gmail.com>
2023-11-13 11:03:16 +0200

    Separate path exclusion form dialog to an independent component

--- apps/curation_front_end/src/components/ExclusionFormDialog.tsx
+++ apps/curation_front_end/src/components/ExclusionFormDialog.tsx
@@ -2,44 +2,38 @@
 //
 // SPDX-License-Identifier: MIT
 
-import React, { useState, useEffect } from "react";
-import {
-    Dialog,
-    DialogTrigger,
-    DialogContent,
-    DialogFooter,
-} from "@/components/ui/dialog";
+import React from "react";
+import { Dialog, DialogContent, DialogFooter } from "@/components/ui/dialog";
 import ExclusionForm from "@/components/ExclusionForm";
-import { DialogClose } from "@radix-ui/react-dialog";
 import { Button } from "@/components/ui/button";
 
 type Props = {
     purl: string | undefined;
     pattern?: string;
+    open: boolean;
+    setOpen: (open: boolean) => void;
 };
 
-const ExclusionFormDialog = ({ purl, pattern }: Props) => {
-    const [open, setOpen] = useState<boolean>(true);
-
-    // Log the value of open to the console
-    useEffect(() => {
-        console.log(open);
-    }, [open]);
-
+const ExclusionFormDialog = ({ purl, pattern, open, setOpen }: Props) => {
     return (
-        <DialogContent>
-            <ExclusionForm purl={purl} pattern={pattern} setOpen={setOpen} />
-            <DialogFooter className="flex justify-end">
-                <DialogClose asChild>
+        <Dialog open={open}>
+            <DialogContent>
+                <ExclusionForm
+                    purl={purl}
+                    pattern={pattern}
+                    setOpen={setOpen}
+                />
+                <DialogFooter className="flex justify-end">
                     <Button
                         variant="outline"
                         className="text-xs p-1 rounded-md"
+                        onClick={() => setOpen(false)}
                     >
-                        Close
+                        Cancel
                     </Button>
-                </DialogClose>
-            </DialogFooter>
-        </DialogContent>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     );
 };
 
