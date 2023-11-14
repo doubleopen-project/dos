259abebfc15e5717b676d73e2a089c8e2ea1d423
Etsija <jyrki.keisala@gmail.com>
2023-11-14 13:14:13 +0200

    Change path exclusion component type, use shadcn alert dialogs for confirm

---
+++ apps/curation_front_end/src/components/path_exclusions/ExclusionDB.tsx
@@ -0,0 +1,167 @@
+// SPDX-FileCopyrightText: 2023 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+import React, { useState, useEffect, useRef } from "react";
+import { userHooks } from "@/hooks/zodiosHooks";
+import { useUser } from "@/hooks/useUser";
+import { Loader2 } from "lucide-react";
+import { ChevronsUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+} from "@/components/ui/command";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import QueryDeleteButton from "@/components/QueryDeleteButton";
+
+type Props = {
+    purl: string | undefined;
+    fractionalWidth: number;
+};
+
+const ExclusionDB = ({ purl, fractionalWidth = 0.75 }: Props) => {
+    const [open, setOpen] = useState(false);
+    const [listWidth, setListWidth] = useState(0);
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const { data, isLoading, error } = userHooks.useImmutableQuery(
+        "/path-exclusions",
+        { purl: purl as string },
+        { withCredentials: true },
+        { enabled: !!purl },
+    );
+
+    useEffect(() => {
+        if (buttonRef.current) {
+            const width = buttonRef.current.offsetWidth;
+            setListWidth(width * fractionalWidth);
+        }
+    }, [buttonRef.current?.offsetWidth]); // Re-run effect if the button's size changes
+
+    // Get user from useUser hook, to decide what DB rights the user has for curations
+    let user = undefined;
+    user = useUser({});
+    const userName = user ? user.username : "Guest";
+    const userRole = user ? user.role : "";
+
+    return (
+        <>
+            {isLoading && (
+                <div className="flex items-center justify-center h-full">
+                    <Loader2 className="w-16 h-16 mr-2 animate-spin" />
+                </div>
+            )}
+            {error && <div>{error.message}</div>}
+            {data && (
+                <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                        <Button
+                            ref={buttonRef}
+                            variant="outline"
+                            role="combobox"
+                            aria-expanded={open}
+                            className="justify-between w-full h-fit"
+                        >
+                            <div className="text-xs">
+                                List/delete path exclusions
+                            </div>
+                            <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                        className="p-0"
+                        style={{ width: listWidth }}
+                    >
+                        <Command>
+                            <CommandInput placeholder="Search for path exclusion..." />
+                            <CommandEmpty>
+                                No path exclusions found.
+                            </CommandEmpty>
+                            <CommandGroup className="max-h-[70vh] min-h-[1px] overflow-y-auto">
+                                {data.pathExclusions.map((exclusion) => (
+                                    <div
+                                        key={`wrapper-${exclusion.id}`}
+                                        className="flex items-start justify-between"
+                                    >
+                                        <CommandItem
+                                            key={exclusion.id}
+                                            className="items-start flex-1 text-left"
+                                        >
+                                            <div className="flex flex-col w-full text-xs">
+                                                <span className="flex justify-between mb-2">
+                                                    <span>
+                                                        <span className="mr-1">
+                                                            Glob:
+                                                        </span>
+                                                        <span className="p-1 font-bold bg-green-400 rounded-sm">
+                                                            {exclusion.pattern}
+                                                        </span>
+                                                    </span>
+                                                    <span className="">
+                                                        <span className="mr-1">
+                                                            {
+                                                                new Date(
+                                                                    exclusion.updatedAt,
+                                                                )
+                                                                    .toISOString()
+                                                                    .split(
+                                                                        "T",
+                                                                    )[0]
+                                                            }
+                                                        </span>
+                                                        <span className="p-1 font-bold bg-orange-400 rounded-sm">
+                                                            {
+                                                                exclusion.user
+                                                                    .username
+                                                            }
+                                                        </span>
+                                                    </span>
+                                                </span>
+                                                <span className="text-smaller">
+                                                    <span className="mr-1">
+                                                        Reason:
+                                                    </span>
+                                                    <span>
+                                                        {exclusion.reason}
+                                                    </span>
+                                                </span>
+                                                <span className="italic text-smaller">
+                                                    {exclusion.comment}
+                                                </span>
+                                            </div>
+                                        </CommandItem>
+                                        <CommandItem
+                                            key={`delete-${exclusion.id}`}
+                                            className="items-start text-left"
+                                        >
+                                            {(userName ===
+                                                exclusion.user.username ||
+                                                userRole === "ADMIN") && (
+                                                <QueryDeleteButton
+                                                    id={exclusion.id}
+                                                    data={exclusion.pattern}
+                                                    deleteQuery={
+                                                        "/path-exclusion/:id"
+                                                    }
+                                                />
+                                            )}
+                                        </CommandItem>
+                                    </div>
+                                ))}
+                            </CommandGroup>
+                        </Command>
+                    </PopoverContent>
+                </Popover>
+            )}
+        </>
+    );
+};
+
+export default ExclusionDB;
