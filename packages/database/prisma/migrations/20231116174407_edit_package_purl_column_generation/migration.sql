9feca8657dc28a2895f09bc9e628f59e7bf29e4c
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-11-16 22:04:06 +0200

    Edit package url generate SQL script

---
+++ packages/database/prisma/migrations/20231116174407_edit_package_purl_column_generation/migration.sql
@@ -0,0 +1,300 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[purl]` on the table `Package` will be added. If there are existing duplicate values, this will fail.
+
+*/
+
+ALTER TABLE "Package"
+ALTER COLUMN "version" DROP NOT NULL,
+DROP COLUMN "purl",
+ADD COLUMN "purl" TEXT NOT NULL GENERATED ALWAYS AS (
+  'pkg:' 
+  || "type" 
+  || (CASE 
+        WHEN "namespace" IS NOT NULL THEN '/' ||
+        Replace(
+          --Replace(
+            Replace(
+              Replace(
+                Replace(
+                  Replace(
+                    Replace(
+                      Replace(
+                        Replace(
+                          Replace(
+                            Replace(
+                              Replace(
+                                Replace(
+                                  Replace(
+                                    Replace(
+                                      Replace(
+                                        Replace(
+                                          Replace(
+                                            Replace(
+                                              Replace(
+                                                Replace(
+                                                  --Replace(
+                                                    Replace(
+                                                      Replace(
+                                                        Replace(
+                                                          Replace(
+                                                            Replace("namespace", '%', '%25')
+                                                          , '&', '%26')
+                                                        , '$', '%24')
+                                                      , '+', '%2B')
+                                                    , ',', '%2C')
+                                                  --, ':', '%3A')
+                                                , ';', '%3B')
+                                              , '=', '%3D')
+                                            , '?', '%3F')
+                                          , '@', '%40')
+                                        , '#', '%23')
+                                      , '<', '%3C')
+                                    , '>', '%3E')
+                                  , '[', '%5B')
+                                , ']', '%5D')
+                              , '{', '%7B')
+                            , '}', '%7D')
+                          , '|', '%7C')
+                        , '^', '%5E')
+                      , ' ', '%20')
+                    , '~', '%7E')
+                  , '`', '%60')
+                , '*', '%2A')
+              , '(', '%28')
+            , ')', '%29')
+          --, '/', '%2F')
+        , '\', '%5C')
+        ELSE ''
+      END) 
+  || '/' 
+  || Replace(
+          Replace(
+            Replace(
+              Replace(
+                Replace(
+                  Replace(
+                    Replace(
+                      Replace(
+                        Replace(
+                          Replace(
+                            Replace(
+                              Replace(
+                                Replace(
+                                  Replace(
+                                    Replace(
+                                      Replace(
+                                        Replace(
+                                          Replace(
+                                            Replace(
+                                              Replace(
+                                                Replace(
+                                                  --Replace(
+                                                    Replace(
+                                                      Replace(
+                                                        Replace(
+                                                          Replace(
+                                                            Replace("name", '%', '%25')
+                                                          , '&', '%26')
+                                                        , '$', '%24')
+                                                      , '+', '%2B')
+                                                    , ',', '%2C')
+                                                  --, ':', '%3A')
+                                                , ';', '%3B')
+                                              , '=', '%3D')
+                                            , '?', '%3F')
+                                          , '@', '%40')
+                                        , '#', '%23')
+                                      , '<', '%3C')
+                                    , '>', '%3E')
+                                  , '[', '%5B')
+                                , ']', '%5D')
+                              , '{', '%7B')
+                            , '}', '%7D')
+                          , '|', '%7C')
+                        , '^', '%5E')
+                      , ' ', '%20')
+                    , '~', '%7E')
+                  , '`', '%60')
+                , '*', '%2A')
+              , '(', '%28')
+            , ')', '%29')
+          , '/', '%2F')
+        , '\', '%5C') 
+  || (CASE 
+        WHEN "version" IS NOT NULL THEN '@' ||
+        Replace(
+          Replace(
+            Replace(
+              Replace(
+                Replace(
+                  Replace(
+                    Replace(
+                      Replace(
+                        Replace(
+                          Replace(
+                            Replace(
+                              Replace(
+                                Replace(
+                                  Replace(
+                                    Replace(
+                                      Replace(
+                                        Replace(
+                                          Replace(
+                                            Replace(
+                                              Replace(
+                                                Replace(
+                                                  --Replace(
+                                                    Replace(
+                                                      Replace(
+                                                        Replace(
+                                                          Replace(
+                                                            Replace("version", '%', '%25')
+                                                          , '&', '%26')
+                                                        , '$', '%24')
+                                                      , '+', '%2B')
+                                                    , ',', '%2C')
+                                                  --, ':', '%3A')
+                                                , ';', '%3B')
+                                              , '=', '%3D')
+                                            , '?', '%3F')
+                                          , '@', '%40')
+                                        , '#', '%23')
+                                      , '<', '%3C')
+                                    , '>', '%3E')
+                                  , '[', '%5B')
+                                , ']', '%5D')
+                              , '{', '%7B')
+                            , '}', '%7D')
+                          , '|', '%7C')
+                        , '^', '%5E')
+                      , ' ', '%20')
+                    , '~', '%7E')
+                  , '`', '%60')
+                , '*', '%2A')
+              , '(', '%28')
+            , ')', '%29')
+          , '/', '%2F')
+        , '\', '%5C')
+        ELSE '' 
+      END) 
+  || (CASE 
+        WHEN "qualifiers" IS NOT NULL THEN '?' ||
+        Replace(
+          --Replace(
+            Replace(
+              Replace(
+                Replace(
+                  Replace(
+                    Replace(
+                      Replace(
+                        Replace(
+                          Replace(
+                            Replace(
+                              Replace(
+                                Replace(
+                                  Replace(
+                                    Replace(
+                                      Replace(
+                                        Replace(
+                                          Replace(
+                                            Replace(
+                                              --Replace(
+                                                Replace(
+                                                  --Replace(
+                                                    Replace(
+                                                      Replace(
+                                                        Replace(
+                                                          --Replace(
+                                                            Replace("qualifiers", '%', '%25')
+                                                          --, '&', '%26')
+                                                        , '$', '%24')
+                                                      , '+', '%2B')
+                                                    , ',', '%2C')
+                                                  --, ':', '%3A')
+                                                , ';', '%3B')
+                                              --, '=', '%3D')
+                                            , '?', '%3F')
+                                          , '@', '%40')
+                                        , '#', '%23')
+                                      , '<', '%3C')
+                                    , '>', '%3E')
+                                  , '[', '%5B')
+                                , ']', '%5D')
+                              , '{', '%7B')
+                            , '}', '%7D')
+                          , '|', '%7C')
+                        , '^', '%5E')
+                      , ' ', '%20')
+                    , '~', '%7E')
+                  , '`', '%60')
+                , '*', '%2A')
+              , '(', '%28')
+            , ')', '%29')
+          --, '/', '%2F')
+        , '\', '%5C')
+        ELSE '' 
+      END) 
+  || (CASE 
+        WHEN "subpath" IS NOT NULL THEN '#' || 
+        Replace(
+          --Replace(
+            Replace(
+              Replace(
+                Replace(
+                  Replace(
+                    Replace(
+                      Replace(
+                        Replace(
+                          Replace(
+                            Replace(
+                              Replace(
+                                Replace(
+                                  Replace(
+                                    Replace(
+                                      Replace(
+                                        Replace(
+                                          Replace(
+                                            Replace(
+                                              Replace(
+                                                Replace(
+                                                  --Replace(
+                                                    Replace(
+                                                      Replace(
+                                                        Replace(
+                                                          Replace(
+                                                            Replace("subpath", '%', '%25')
+                                                          , '&', '%26')
+                                                        , '$', '%24')
+                                                      , '+', '%2B')
+                                                    , ',', '%2C')
+                                                  --, ':', '%3A')
+                                                , ';', '%3B')
+                                              , '=', '%3D')
+                                            , '?', '%3F')
+                                          , '@', '%40')
+                                        , '#', '%23')
+                                      , '<', '%3C')
+                                    , '>', '%3E')
+                                  , '[', '%5B')
+                                , ']', '%5D')
+                              , '{', '%7B')
+                            , '}', '%7D')
+                          , '|', '%7C')
+                        , '^', '%5E')
+                      , ' ', '%20')
+                    , '~', '%7E')
+                  , '`', '%60')
+                , '*', '%2A')
+              , '(', '%28')
+            , ')', '%29')
+          --, '/', '%2F')
+        , '\', '%5C') 
+        ELSE '' 
+      END)
+) STORED;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Package_purl_key" ON "Package"("purl");
+
