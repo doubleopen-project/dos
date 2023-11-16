a5724e3f69fdaeeb55be96f92bc28d8ebc9822ae
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2023-11-16 18:45:43 +0200

    Add easy deselecting license selections and emptying SPDX expression field

--- apps/curation_front_end/src/components/license_conclusions/CurationDB.tsx
+++ apps/curation_front_end/src/components/license_conclusions/CurationDB.tsx
@@ -16,10 +16,16 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { useUser } from "@/hooks/useUser";
 import { cn } from "@/lib/utils";
 import { ZodiosResponseByPath } from "@zodios/core";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Check, ChevronsUpDown, XCircle } from "lucide-react";
 import { useRouter } from "next/router";
 import React, { useEffect, useRef, useState } from "react";
 import { userAPI } from "validation-helpers";
@@ -76,121 +82,150 @@ const CurationDB = ({
     };
 
     return (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-                <Button
-                    ref={buttonRef}
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className="justify-between w-full h-fit"
-                >
-                    <div className="text-xs">
-                        {router.isReady
-                            ? value
-                                ? data?.licenseConclusions.find(
-                                      (d) => d.id === parseInt(value, 10),
-                                  )?.concludedLicenseExpressionSPDX
-                                : "Select curation from DB..."
-                            : null}
-                    </div>
-                    <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="p-0" style={{ width: listWidth }}>
-                <Command>
-                    <CommandInput placeholder="Search curation..." />
-                    <CommandEmpty>No curations found.</CommandEmpty>
-                    <CommandGroup className="max-h-[70vh] min-h-[1px] overflow-y-auto">
-                        {data?.licenseConclusions.map((d) => (
-                            <div
-                                key={`wrapper-${d.id}`}
-                                className="flex items-start justify-between"
-                            >
-                                <CommandItem
-                                    key={d.id}
-                                    className="items-start flex-1 text-left"
-                                    onSelect={() => handleSelect(d)}
+        <div className="flex">
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button
+                        ref={buttonRef}
+                        variant="outline"
+                        role="combobox"
+                        aria-expanded={open}
+                        className="justify-between w-full h-fit"
+                    >
+                        <div className="text-xs">
+                            {router.isReady
+                                ? value
+                                    ? data?.licenseConclusions.find(
+                                          (d) => d.id === parseInt(value, 10),
+                                      )?.concludedLicenseExpressionSPDX
+                                    : "Select curation from DB..."
+                                : null}
+                        </div>
+                        <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="p-0" style={{ width: listWidth }}>
+                    <Command>
+                        <CommandInput placeholder="Search curation..." />
+                        <CommandEmpty>No curations found.</CommandEmpty>
+                        <CommandGroup className="max-h-[70vh] min-h-[1px] overflow-y-auto">
+                            {data?.licenseConclusions.map((d) => (
+                                <div
+                                    key={`wrapper-${d.id}`}
+                                    className="flex items-start justify-between"
                                 >
-                                    <Check
-                                        className={cn(
-                                            "mr-2 h-4 w-4",
-                                            value === d.id.toString()
-                                                ? "opacity-100"
-                                                : "opacity-0",
-                                        )}
-                                    />
-                                    <div className="flex flex-col w-full ml-2 text-xs">
-                                        <span className="flex justify-between mb-2">
-                                            <span>
-                                                <span className="mr-1">
-                                                    Curated:
+                                    <CommandItem
+                                        key={d.id}
+                                        className="items-start flex-1 text-left"
+                                        onSelect={() => handleSelect(d)}
+                                    >
+                                        <Check
+                                            className={cn(
+                                                "mr-2 h-4 w-4",
+                                                value === d.id.toString()
+                                                    ? "opacity-100"
+                                                    : "opacity-0",
+                                            )}
+                                        />
+                                        <div className="flex flex-col w-full ml-2 text-xs">
+                                            <span className="flex justify-between mb-2">
+                                                <span>
+                                                    <span className="mr-1">
+                                                        Curated:
+                                                    </span>
+                                                    <span className="p-1 font-bold bg-green-400 rounded-sm">
+                                                        {
+                                                            d.concludedLicenseExpressionSPDX
+                                                        }
+                                                    </span>
                                                 </span>
-                                                <span className="p-1 font-bold bg-green-400 rounded-sm">
+                                                <span className="">
+                                                    <span className="mr-1">
+                                                        {
+                                                            new Date(
+                                                                d.updatedAt,
+                                                            )
+                                                                .toISOString()
+                                                                .split("T")[0]
+                                                        }
+                                                    </span>
+                                                    <span className="p-1 font-bold bg-orange-400 rounded-sm">
+                                                        {d.user.username}
+                                                    </span>
+                                                </span>
+                                            </span>
+                                            <span className="text-smaller">
+                                                <span className="mr-1">
+                                                    Detected:
+                                                </span>
+                                                <span>
                                                     {
+                                                        d.detectedLicenseExpressionSPDX
+                                                    }
+                                                </span>
+                                            </span>
+                                            <span className="text-smaller">
+                                                <span className="mr-1">
+                                                    Context PURL:
+                                                </span>
+                                                {d.contextPurl}
+                                            </span>
+                                            <span className="italic text-smaller">
+                                                {d.comment}
+                                            </span>
+                                        </div>
+                                    </CommandItem>
+                                    <CommandItem
+                                        key={`delete-${d.id}`}
+                                        className="items-start text-left"
+                                    >
+                                        {(userName === d.user.username ||
+                                            userRole === "ADMIN") && (
+                                            <>
+                                                <QueryDeleteButton
+                                                    id={d.id}
+                                                    data={
                                                         d.concludedLicenseExpressionSPDX
                                                     }
-                                                </span>
-                                            </span>
-                                            <span className="">
-                                                <span className="mr-1">
-                                                    {
-                                                        new Date(d.updatedAt)
-                                                            .toISOString()
-                                                            .split("T")[0]
+                                                    deleteQuery={
+                                                        "/license-conclusion/:id"
                                                     }
-                                                </span>
-                                                <span className="p-1 font-bold bg-orange-400 rounded-sm">
-                                                    {d.user.username}
-                                                </span>
-                                            </span>
-                                        </span>
-                                        <span className="text-smaller">
-                                            <span className="mr-1">
-                                                Detected:
-                                            </span>
-                                            <span>
-                                                {
-                                                    d.detectedLicenseExpressionSPDX
-                                                }
-                                            </span>
-                                        </span>
-                                        <span className="text-smaller">
-                                            <span className="mr-1">
-                                                Context PURL:
-                                            </span>
-                                            {d.contextPurl}
-                                        </span>
-                                        <span className="italic text-smaller">
-                                            {d.comment}
-                                        </span>
-                                    </div>
-                                </CommandItem>
-                                <CommandItem
-                                    key={`delete-${d.id}`}
-                                    className="items-start text-left"
-                                >
-                                    {(userName === d.user.username ||
-                                        userRole === "ADMIN") && (
-                                        <>
-                                            <QueryDeleteButton
-                                                id={d.id}
-                                                data={
-                                                    d.concludedLicenseExpressionSPDX
-                                                }
-                                                deleteQuery={
-                                                    "/license-conclusion/:id"
-                                                }
-                                            />
-                                        </>
-                                    )}
-                                </CommandItem>
-                            </div>
-                        ))}
-                    </CommandGroup>
-                </Command>
-            </PopoverContent>
-        </Popover>
+                                                />
+                                            </>
+                                        )}
+                                    </CommandItem>
+                                </div>
+                            ))}
+                        </CommandGroup>
+                    </Command>
+                </PopoverContent>
+            </Popover>
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger
+                        className="ml-1"
+                        type="button"
+                        onClick={() => {
+                            setValue("");
+                            setConcludedLicenseExpressionSPDX("");
+                        }}
+                        disabled={!value || value === ""}
+                    >
+                        <XCircle
+                            className={cn(
+                                "mx-2 text-gray-400 h-fit",
+                                !value || value.length === 0
+                                    ? "opacity-40"
+                                    : "opacity-100",
+                            )}
+                        />
+                    </TooltipTrigger>
+                    <TooltipContent side="right">
+                        Clear selection
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+        </div>
     );
 };
 
