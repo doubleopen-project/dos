8c3f949629e51fd90fe97cd591d1acacd81058c9
Etsija <jyrki.keisala@gmail.com>
2023-12-12 15:26:50 +0200

    Add license conclusion table, 1st version

---
+++ apps/curation_front_end/src/components/license_conclusion_table/LicenseConclusionList.tsx
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: 2023 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+import React from "react";
+import { ZodiosResponseByPath } from "@zodios/core";
+import { userAPI } from "validation-helpers";
+import { userHooks } from "@/hooks/zodiosHooks";
+import { columns } from "@/components/license_conclusion_table/columns";
+import { DataTable } from "@/components/license_conclusion_table/DataTable";
+
+type LicenseConclusionListProps = {
+    user: ZodiosResponseByPath<typeof userAPI, "get", "/user">;
+};
+
+const LicenseConclusionList = ({ user }: LicenseConclusionListProps) => {
+    const { data, isLoading, error } = userHooks.useGet("/license-conclusion", {
+        withCredentials: true,
+    });
+    if (isLoading) {
+        return <div>Loading license conclusions...</div>;
+    }
+    if (error) return <div>{error.message}</div>;
+    if (!data) return <div>No data</div>;
+
+    // Get user role, to decide what rights the user has for this view
+    const tableColumns = columns(user);
+
+    return (
+        <div className="container mx-auto">
+            {user && (
+                <DataTable
+                    columns={tableColumns}
+                    data={data.licenseConclusions}
+                />
+            )}
+        </div>
+    );
+};
+
+export default LicenseConclusionList;
