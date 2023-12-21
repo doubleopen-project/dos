34a4edccfbe6407b852ebcb28a8b2b6a1d0cc84b
Mikko Murto <mikko.murto@hhpartners.fi>
2023-12-21 15:12:51 +0200

    test: save playwright videos on first retry

--- apps/clearance_ui/playwright.config.ts
+++ apps/clearance_ui/playwright.config.ts
@@ -34,6 +34,8 @@ export default defineConfig({
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
+
+        video: "on-first-retry",
     },
 
     /* Configure projects for major browsers */
