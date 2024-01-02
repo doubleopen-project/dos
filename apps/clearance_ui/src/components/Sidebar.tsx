01980271b560b7351d2f1119c19dbe052d64ab39
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-01-02 15:32:33 +0200

    refactor(ui): Separate main from Sidebar component
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- apps/clearance_ui/src/components/Sidebar.tsx
+++ apps/clearance_ui/src/components/Sidebar.tsx
@@ -16,88 +16,81 @@ import {
 import { useUser } from "@/hooks/useUser";
 import { ModeToggle } from "@/components/ModeToggle";
 
-interface SidebarProps {
-    children: React.ReactNode;
-}
-
-const Sidebar = ({ children }: SidebarProps) => {
+const Sidebar = () => {
     const user = useUser();
     return (
-        <div className="flex">
-            <div className="overflow-none flex h-screen flex-col items-center justify-between border-r-[1px] p-4">
-                <div className="flex flex-col items-center">
-                    <Link href="/">
+        <div className="overflow-none flex h-screen flex-col items-center justify-between border-r-[1px] p-4">
+            <div className="flex flex-col items-center">
+                <Link href="/">
+                    <div
+                        className="inline-block rounded-lg bg-purple-800 p-3 text-white hover:bg-purple-400"
+                        title="Home"
+                    >
+                        <AiOutlineHome size={20} />
+                    </div>
+                </Link>
+                <span className="w-full border-b-[1px] border-gray-200 p-2"></span>
+                {user && (
+                    <>
+                        <Link href="/packages">
+                            <div
+                                className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
+                                title="Package Library"
+                            >
+                                <GrCatalog size={20} />
+                            </div>
+                        </Link>
+                        <Link href="/clearances">
+                            <div
+                                className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
+                                title="Clearance Library"
+                            >
+                                <GrInspect size={20} />
+                            </div>
+                        </Link>
+
+                        <Link href="/settings">
+                            <div
+                                className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
+                                title="Settings"
+                            >
+                                <GrUserSettings size={20} />
+                            </div>
+                        </Link>
+                    </>
+                )}
+                {user?.role === "ADMIN" && (
+                    <Link href="/admin/users">
                         <div
-                            className="inline-block rounded-lg bg-purple-800 p-3 text-white hover:bg-purple-400"
-                            title="Home"
+                            className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
+                            title="User Management"
                         >
-                            <AiOutlineHome size={20} />
+                            <GrUserManager size={20} />
                         </div>
                     </Link>
-                    <span className="w-full border-b-[1px] border-gray-200 p-2"></span>
-                    {user && (
-                        <>
-                            <Link href="/packages">
-                                <div
-                                    className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
-                                    title="Package Library"
-                                >
-                                    <GrCatalog size={20} />
-                                </div>
-                            </Link>
-                            <Link href="/clearances">
-                                <div
-                                    className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
-                                    title="Clearance Library"
-                                >
-                                    <GrInspect size={20} />
-                                </div>
-                            </Link>
-
-                            <Link href="/settings">
-                                <div
-                                    className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
-                                    title="Settings"
-                                >
-                                    <GrUserSettings size={20} />
-                                </div>
-                            </Link>
-                        </>
-                    )}
-                    {user?.role === "ADMIN" && (
-                        <Link href="/admin/users">
-                            <div
-                                className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
-                                title="User Management"
-                            >
-                                <GrUserManager size={20} />
-                            </div>
-                        </Link>
-                    )}
-                    {!user && (
-                        <Link href="/login">
-                            <div
-                                className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
-                                title="Login"
-                            >
-                                <GrLogin size={20} />
-                            </div>
-                        </Link>
-                    )}
-                    {user && (
-                        <Link href="/logout">
-                            <div
-                                className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
-                                title="Logout"
-                            >
-                                <GrLogout size={20} />
-                            </div>
-                        </Link>
-                    )}
-                    <ModeToggle />
-                </div>
+                )}
+                {!user && (
+                    <Link href="/login">
+                        <div
+                            className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
+                            title="Login"
+                        >
+                            <GrLogin size={20} />
+                        </div>
+                    </Link>
+                )}
+                {user && (
+                    <Link href="/logout">
+                        <div
+                            className="my-4 inline-block rounded-lg bg-gray-200 p-3 text-black hover:bg-gray-400"
+                            title="Logout"
+                        >
+                            <GrLogout size={20} />
+                        </div>
+                    </Link>
+                )}
+                <ModeToggle />
             </div>
-            <main className="w-full overflow-auto">{children}</main>
         </div>
     );
 };
