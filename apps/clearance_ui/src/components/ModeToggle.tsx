da4fcff45bd6a148de49098b097bdf9609d05bb2
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-01-07 19:37:01 +0200

    refactor(ui): Add margin for theme ModeToggle
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- apps/clearance_ui/src/components/ModeToggle.tsx
+++ apps/clearance_ui/src/components/ModeToggle.tsx
@@ -13,8 +13,13 @@ import {
     DropdownMenuRadioItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 
-export function ModeToggle() {
+type ModeToggleProps = {
+    className?: string;
+};
+
+export function ModeToggle({ className }: ModeToggleProps) {
     const { setTheme } = useTheme();
     //const appTheme = useSettingsStore((state) => state.appTheme);
     //const setAppTheme = useSettingsStore((state) => state.setAppTheme);
@@ -22,7 +27,7 @@ export function ModeToggle() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="sm">
+                <Button variant="outline" size="sm" className={cn(className)}>
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                     <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                     <span className="sr-only">Toggle theme</span>
