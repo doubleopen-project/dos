c62ce29747e6157b68b962ba1a26621e5cff2a22
Etsija <jyrki.keisala@gmail.com>
2024-01-08 13:58:07 +0200

    test(ui): Fix license conclusion tests
    Add one test ID for the license conclusion items in the combo box.
    Also change the test logic to use comment field instead of the license,
    and apply chaining filter to the locator, to find exactly the correct
    item, in case some other tests still haven't deleted their items by the
    time this test is run.
    
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/tests/e2e/logged-as-test-user/license-conclusion.spec.ts
+++ apps/clearance_ui/tests/e2e/logged-as-test-user/license-conclusion.spec.ts
@@ -8,24 +8,27 @@ test.describe.configure({ mode: "serial" });
 
 test.beforeEach(async ({ page }) => {
     await page.goto("/packages/pkg%3Ageneric%2Fdos-monorepo%400.0.0");
-    await page.getByRole("link", { name: "package-lock.json" }).click();
 });
 
 test("create license conclusion, delete from Main UI", async ({ page }) => {
+    const license = "MIT";
+    const comment = "Test create license conclusion, delete from Main UI";
+
     // Create a license conclusion
+    await page.getByRole("link", { name: "package.json" }).click();
     await page
         .locator("form button")
         .filter({ hasText: "Select license..." })
         .click();
     await page.getByPlaceholder("Search license...").click();
-    await page.getByPlaceholder("Search license...").fill("MIT");
-    await page.getByRole("option", { name: "MIT", exact: true }).click();
+    await page.getByPlaceholder("Search license...").fill(license);
+    await page.getByRole("option", { name: license, exact: true }).click();
     await page
         .getByPlaceholder("Comment on your license conclusion...")
         .click();
     await page
         .getByPlaceholder("Comment on your license conclusion...")
-        .fill("Test comment 1");
+        .fill(comment);
     page.once("dialog", (dialog) => {
         dialog.accept().catch(() => {});
     });
@@ -41,14 +44,16 @@ test("create license conclusion, delete from Main UI", async ({ page }) => {
 
     // Delete the same conclusion
     await page.getByTestId("conclusion-db-button").click();
-    await expect(page.getByTestId("concluded-license").first()).toContainText(
-        "MIT",
-    );
-    await page.getByRole("group").getByRole("button").first().click();
-    await expect(page.getByLabel("Delete").getByText("MIT")).toContainText(
-        "MIT",
+    await page
+        .getByTestId("license-conclusion")
+        .filter({ hasText: comment })
+        .getByTestId("delete-clearance-button")
+        .click();
+    await expect(page.getByLabel("Delete").getByText(license)).toContainText(
+        license,
     );
     await page.getByRole("button", { name: "Delete" }).click();
+
     // Wait for the toast to appear, contain a success text and disappear
     const toastDeleted = page.getByRole("status").first();
     await toastDeleted.waitFor({ state: "visible" });
@@ -62,20 +67,22 @@ test("create license conclusion, delete from Main UI", async ({ page }) => {
 test("create license conclusion, delete from Clearance Library", async ({
     page,
 }) => {
+    const license = "Apache-2.0";
+    const comment =
+        "Test create license conclusion, delete from Clearance Library";
+
     // Create a license conclusion
     await page.getByText("apps").click();
     await page.getByText("curation_front_end").click();
     await page.getByRole("link", { name: "next.config.js" }).click();
     await page.getByPlaceholder("Write your SPDX expression").click();
-    await page
-        .getByPlaceholder("Write your SPDX expression")
-        .fill("Apache-2.0");
+    await page.getByPlaceholder("Write your SPDX expression").fill(license);
     await page
         .getByPlaceholder("Comment on your license conclusion...")
         .click();
     await page
         .getByPlaceholder("Comment on your license conclusion...")
-        .fill("Test comment 2");
+        .fill(comment);
     page.once("dialog", (dialog) => {
         dialog.accept().catch(() => {});
     });
@@ -92,23 +99,14 @@ test("create license conclusion, delete from Clearance Library", async ({
     // Delete the same conclusion
     await page.goto("/clearances");
     await page.getByTestId("clearance-lib-lic-conclusions").click();
-    await expect(
-        page.getByRole("cell", { name: "Apache-" }).first(),
-    ).toContainText("Apache-2.0");
-    await expect(
-        page.getByRole("cell", { name: "Test comment 2" }).first(),
-    ).toContainText("Test comment 2");
-    const rows = await page.$$('tr:has-text("Test comment 2")');
-    const row = rows[0];
-    const button = row
-        ? await row.$('[data-testid="delete-clearance-button"]')
-        : null;
-    if (button) {
-        await button.click();
-    }
-    await expect(
-        page.getByLabel("Delete").getByText("Apache-2.0"),
-    ).toContainText("Apache-2.0");
+    await page
+        .getByRole("row")
+        .filter({ hasText: comment })
+        .getByTestId("delete-clearance-button")
+        .click();
+    await expect(page.getByLabel("Delete").getByText(license)).toContainText(
+        license,
+    );
     await page.getByRole("button", { name: "Delete" }).click();
     // Wait for the toast to appear, contain a success text and disappear
     const toastDeleted = page.getByRole("status").first();
