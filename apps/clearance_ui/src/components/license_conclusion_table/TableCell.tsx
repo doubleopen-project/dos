8578e0c8fa73c09cc652ade8475057f10678ee0a
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-01-09 15:40:04 +0200

    feat(ui): Add revertData and updateData for license conclusion table
    Also add TableCell component to help control input and textarea values
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

---
+++ apps/clearance_ui/src/components/license_conclusion_table/TableCell.tsx
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: 2024 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+import { useEffect, useState } from "react";
+import { CellContext } from "@tanstack/react-table";
+import { ZodiosResponseByAlias } from "@zodios/core";
+import { userAPI } from "validation-helpers";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+type LicenseConclusion = ZodiosResponseByAlias<
+    typeof userAPI,
+    "GetLicenseConclusions"
+>["licenseConclusions"][0];
+
+const TableCell = ({
+    getValue,
+    row,
+    column,
+    table,
+}: CellContext<LicenseConclusion, unknown>) => {
+    const initialValue = getValue();
+    const columnMeta = column.columnDef.meta;
+    const tableMeta = table.options.meta;
+    const [value, setValue] = useState(
+        typeof initialValue === "string" ? initialValue : "",
+    );
+
+    useEffect(() => {
+        setValue(typeof initialValue === "string" ? initialValue : "");
+    }, [initialValue]);
+
+    const onBlur = () => {
+        tableMeta?.updateData(row.index, column.id, value);
+    };
+
+    if (tableMeta?.editedRows[parseInt(row.id)]) {
+        return columnMeta?.type === "textarea" ? (
+            <Textarea
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                onBlur={onBlur}
+            />
+        ) : (
+            <Input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                onBlur={onBlur}
+                type={"text"}
+            />
+        );
+    }
+    return <span>{value}</span>;
+};
+
+export default TableCell;
