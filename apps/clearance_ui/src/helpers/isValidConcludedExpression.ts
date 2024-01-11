ab2d601e1aa054b3513bfdd60d1786d830eabd5e
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-01-11 17:55:04 +0200

    feat(ui): Add details in the error toast description
    Adds details where to find the error in if concluded license is invalid
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

---
+++ apps/clearance_ui/src/helpers/isValidConcludedExpression.ts
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: 2024 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+import { parseSPDX } from "common-helpers";
+
+export const isValidConcludedExpression = (concludedLicense: string) => {
+    try {
+        parseSPDX(concludedLicense);
+        return {
+            isValid: true,
+            errWord: null,
+        };
+    } catch (error) {
+        if (error instanceof Error) {
+            const errArr = error.message.split(" ");
+            const errIndex = parseInt(errArr[errArr.length - 1]);
+
+            // find word starting from errIndex in concluded expression:
+            const concludedArr = concludedLicense.split(" ");
+            let count = 0;
+            let errWord = null;
+
+            for (const word of concludedArr) {
+                count += word.length;
+                if (count >= errIndex) {
+                    errWord = word;
+                    break;
+                }
+                count += 1; // accounts for the space between words
+            }
+            return {
+                isValid: false,
+                errWord: errWord,
+            };
+        }
+        return {
+            isValid: false,
+            errWord: null,
+        };
+    }
+};
