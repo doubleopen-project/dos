2a192bcb73d888f735315ce8f08315d68d0a2b28
Jyrki Keisala <jyrki.keisala@gmail.com>
2024-01-16 12:51:40 +0200

    feat(ui): Implement package details as an Accordion
    In order to keep the Main UI clean with the new "dirty" PURLs, show
    the package name, version and subpath only in left-top corner of the
    Main UI.  Clicking the package name reveals its details.
    
    Signed-off-by: Jyrki Keisala <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/components/PurlDetails.tsx
+++ apps/clearance_ui/src/components/PurlDetails.tsx
@@ -3,6 +3,13 @@
 // SPDX-License-Identifier: MIT
 
 import React from "react";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
 import { parsePurlAndQualifiers } from "@/helpers/parsePurlAndQualifiers";
 
 type Props = {
@@ -11,15 +18,38 @@ type Props = {
 
 const PurlDetails = ({ purl }: Props) => {
     const parsedPurl = parsePurlAndQualifiers(purl);
+    let packageName = parsedPurl.name + "@" + parsedPurl.version;
+    if (parsedPurl.subpath) {
+        packageName += "/" + parsedPurl.subpath;
+    }
     return (
-        <div>
-            {parsedPurl.qualifiers &&
-                Object.entries(parsedPurl.qualifiers).map(([key, value]) => (
-                    <p key={key}>
-                        <b>{key}:</b> {value}
-                    </p>
-                ))}
-        </div>
+        <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+                <AccordionTrigger>
+                    <Badge className="rounded-md">{packageName}</Badge>
+                </AccordionTrigger>
+                <AccordionContent className="text-xs">
+                    {parsedPurl.type && (
+                        <p className="font-xs">
+                            <b>Type:</b> {parsedPurl.type}
+                        </p>
+                    )}
+                    {parsedPurl.namespace && (
+                        <p className="font-xs">
+                            <b>Namespace:</b> {parsedPurl.namespace}
+                        </p>
+                    )}
+                    {parsedPurl.qualifiers &&
+                        Object.entries(parsedPurl.qualifiers).map(
+                            ([key, value]) => (
+                                <p className="font-xs" key={key}>
+                                    <b>{key}:</b> {value}
+                                </p>
+                            ),
+                        )}
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion>
     );
 };
 
