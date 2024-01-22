8e7bc0b41f3de0f73a4c43420620f2b1ff725cf5
Etsija <jyrki.keisala@gmail.com>
2024-01-22 15:05:30 +0200

    feat(ui): Add helpers to decompose SPDX license expressions
    Add a helper function to decompose a set of SPDX license expressions
    into a unique and sorted list of individual licenses, with possible
    exceptions.
    
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

---
+++ apps/clearance_ui/src/helpers/decomposeLicenses.ts
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: 2024 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+import { parseSPDX } from "common-helpers";
+
+type SPDXExpression = {
+    license?: string;
+    exception?: string;
+    conjunction?: "and" | "or";
+    left?: SPDXExpression;
+    right?: SPDXExpression;
+};
+
+function extractLicenses(expression: SPDXExpression | undefined): string[] {
+    if (!expression) {
+        return [];
+    }
+    const licenses: string[] = [];
+
+    if (expression.license) {
+        if (expression.exception) {
+            licenses.push(`${expression.license} WITH ${expression.exception}`);
+        } else {
+            licenses.push(expression.license);
+        }
+    }
+    if (expression.left) {
+        const leftLicenses = extractLicenses(expression.left);
+        licenses.push(...leftLicenses);
+    }
+    if (expression.right) {
+        const rightLicenses = extractLicenses(expression.right);
+        licenses.push(...rightLicenses);
+    }
+
+    return licenses;
+}
+
+export function decomposeLicenses(spdxExpressions: Set<string>): Set<string> {
+    const allLicenses: string[] = [];
+
+    // Process each SPDX expression and extract licenses
+    spdxExpressions.forEach((spdxExpression) => {
+        console.log(`Processing ${spdxExpression}`);
+        const parsedInfo = parseSPDX(spdxExpression);
+        const licenses = extractLicenses(parsedInfo);
+        allLicenses.push(...licenses);
+    });
+
+    // Deduplicate and sort the licenses alphabetically
+    const uniqueLicenses = Array.from(new Set(allLicenses)).sort();
+
+    return new Set(uniqueLicenses);
+}
