5d4cc4219b14309e2a5c16b39c7edf7195917716
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-01-25 13:44:07 +0200

    chore: Add initial contents from spdx-expression-parse.js
    Contents from doubleopen-project fork. Original package
    <https://github.com/jslicense/spdx-expression-parse.js>
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- packages/spdx-validation/index.d.ts
+++ packages/spdx-validation/index.d.ts
@@ -1,4 +1,13 @@
-// SPDX-FileCopyrightText: 2024 Double Open Oy
-//
-// SPDX-License-Identifier: MIT
+/*
+ * Parser is based on spdx-expression-parse.js <https://github.com/jslicense/spdx-expression-parse.js>, which is licensed under MIT and includes the following copyright holders:
+ * SPDX-FileCopyrightText: 2015 Kyle E. Mitchell <kyle@kemitchell.com> (https://kemitchell.com)
+ * SPDX-FileCopyrightText: 2015 C. Scott Ananian <cscott@cscott.net> (http://cscott.net)
+ * SPDX-FileCopyrightText: 2015 Shinnosuke Watanabe <snnskwtnb@gmail.com>
+ * SPDX-FileCopyrightText: 2015 Antoine Motet <antoine.motet@gmail.com>
+ *
+ *
+ * SPDX-FileCopyrightText: 2024 Double Open Oy <support@doubleopen.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
 export {};
