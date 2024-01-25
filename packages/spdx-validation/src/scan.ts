f1b6afdc0c36d237e378068d4df351734cbc6ce6
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-01-25 13:44:07 +0200

    chore: Add initial contents from spdx-expression-parse.js
    Contents from doubleopen-project fork. Original package
    <https://github.com/jslicense/spdx-expression-parse.js>
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

---
+++ packages/spdx-validation/src/scan.ts
@@ -0,0 +1,146 @@
+/*
+ * Parser is based on spdx-expression-parse.js <https://github.com/jslicense/spdx-expression-parse.js>, which is licensed under MIT and includes the following copyright holders:
+ * SPDX-FileCopyrightText: 2015 Kyle E. Mitchell <kyle@kemitchell.com> (https://kemitchell.com)
+ * SPDX-FileCopyrightText: 2015 C. Scott Ananian <cscott@cscott.net> (http://cscott.net)
+ * SPDX-FileCopyrightText: 2015 Shinnosuke Watanabe <snnskwtnb@gmail.com>
+ * SPDX-FileCopyrightText: 2015 Antoine Motet <antoine.motet@gmail.com>
+ *
+ *
+ * SPDX-FileCopyrightText: 2024 Double Open Oy <support@doubleopen.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+"use strict";
+
+var licenses = [].concat(require("spdx-license-ids"));
+var exceptions = require("spdx-exceptions");
+
+module.exports = function (source) {
+    var index = 0;
+
+    function hasMore() {
+        return index < source.length;
+    }
+
+    // `value` can be a regexp or a string.
+    // If it is recognized, the matching source string is returned and
+    // the index is incremented. Otherwise `undefined` is returned.
+    function read(value) {
+        if (value instanceof RegExp) {
+            var chars = source.slice(index);
+            var match = chars.match(value);
+            if (match) {
+                index += match[0].length;
+                return match[0];
+            }
+        } else {
+            if (source.indexOf(value, index) === index) {
+                index += value.length;
+                return value;
+            }
+        }
+    }
+
+    function skipWhitespace() {
+        read(/[ ]*/);
+    }
+
+    function operator() {
+        var string;
+        var possibilities = [/^WITH/i, /^AND/i, /^OR/i, "(", ")", ":", "+"];
+        for (var i = 0; i < possibilities.length; i++) {
+            string = read(possibilities[i]);
+            if (string) {
+                break;
+            }
+        }
+
+        if (string === "+" && index > 1 && source[index - 2] === " ") {
+            throw new Error("Space before `+`");
+        }
+
+        return (
+            string && {
+                type: "OPERATOR",
+                string: string.toUpperCase(),
+            }
+        );
+    }
+
+    function idstring() {
+        return read(/[A-Za-z0-9-.]+/);
+    }
+
+    function expectIdstring() {
+        var string = idstring();
+        if (!string) {
+            throw new Error("Expected idstring at offset " + index);
+        }
+        return string;
+    }
+
+    function documentRef() {
+        if (read("DocumentRef-")) {
+            var string = expectIdstring();
+            return { type: "DOCUMENTREF", string: string };
+        }
+    }
+
+    function licenseRef() {
+        if (read("LicenseRef-")) {
+            var string = expectIdstring();
+            return { type: "LICENSEREF", string: string };
+        }
+    }
+
+    function identifier() {
+        var begin = index;
+        var string = idstring();
+
+        if (licenses.indexOf(string) !== -1) {
+            return {
+                type: "LICENSE",
+                string: string,
+            };
+        } else if (exceptions.indexOf(string) !== -1) {
+            return {
+                type: "EXCEPTION",
+                string: string,
+            };
+        }
+
+        index = begin;
+    }
+
+    // Tries to read the next token. Returns `undefined` if no token is
+    // recognized.
+    function parseToken() {
+        // Ordering matters
+        return operator() || documentRef() || licenseRef() || identifier();
+    }
+
+    var tokens = [];
+    while (hasMore()) {
+        skipWhitespace();
+        if (!hasMore()) {
+            break;
+        }
+
+        var token = parseToken();
+        if (!token) {
+            throw new Error(
+                "Unexpected `" +
+                    source[index] +
+                    "` at offset " +
+                    index +
+                    " in expression `" +
+                    source.slice(index, index + 20) +
+                    "`",
+            );
+        }
+
+        tokens.push(token);
+    }
+    return tokens;
+};
---
+++ packages/spdx-validation/tests/test.ts
@@ -0,0 +1,119 @@
+/*
+ * Parser is based on spdx-expression-parse.js <https://github.com/jslicense/spdx-expression-parse.js>, which is licensed under MIT and includes the following copyright holders:
+ * SPDX-FileCopyrightText: 2015 Kyle E. Mitchell <kyle@kemitchell.com> (https://kemitchell.com)
+ * SPDX-FileCopyrightText: 2015 C. Scott Ananian <cscott@cscott.net> (http://cscott.net)
+ * SPDX-FileCopyrightText: 2015 Shinnosuke Watanabe <snnskwtnb@gmail.com>
+ * SPDX-FileCopyrightText: 2015 Antoine Motet <antoine.motet@gmail.com>
+ *
+ *
+ * SPDX-FileCopyrightText: 2024 Double Open Oy <support@doubleopen.org>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+"use strict";
+
+var assert = require("assert");
+var p = require("./");
+
+// The spec is unclear about tabs and newlines
+it("forbids tabs and newlines", function () {
+    assert.throws(function () {
+        p("MIT\t");
+    });
+    assert.throws(function () {
+        p("\nMIT");
+    });
+});
+
+it("allows many spaces", function () {
+    assert.deepStrictEqual(p(" MIT"), { license: "MIT" });
+
+    assert.deepStrictEqual(p("MIT "), { license: "MIT" });
+
+    assert.deepStrictEqual(p("MIT  AND    BSD-3-Clause"), {
+        left: { license: "MIT" },
+        conjunction: "and",
+        right: { license: "BSD-3-Clause" },
+    });
+});
+
+it("forbids spaces between a license-id and a following `+`", function () {
+    assert.throws(function () {
+        p("MIT +");
+    }, /Space before `\+`/);
+});
+
+it("parses DocumentRefs and LicenseRefs", function () {
+    assert.deepStrictEqual(p("LicenseRef-something"), {
+        license: "LicenseRef-something",
+    });
+
+    assert.deepStrictEqual(
+        p("DocumentRef-spdx-tool-1.2 : LicenseRef-MIT-Style-2"),
+        { license: "DocumentRef-spdx-tool-1.2:LicenseRef-MIT-Style-2" },
+    );
+});
+
+// See the note in `parser.js`.
+it("parses `AND`, `OR` and `WITH` with the correct precedence", function () {
+    assert.deepStrictEqual(p("MIT AND BSD-3-Clause AND CC-BY-4.0"), {
+        left: { license: "MIT" },
+        conjunction: "and",
+        right: {
+            left: { license: "BSD-3-Clause" },
+            conjunction: "and",
+            right: { license: "CC-BY-4.0" },
+        },
+    });
+
+    assert.deepStrictEqual(
+        p(
+            "MIT AND BSD-3-Clause WITH GCC-exception-3.1 OR CC-BY-4.0 AND Apache-2.0",
+        ),
+        {
+            left: {
+                left: { license: "MIT" },
+                conjunction: "and",
+                right: {
+                    license: "BSD-3-Clause",
+                    exception: "GCC-exception-3.1",
+                },
+            },
+            conjunction: "or",
+            right: {
+                left: { license: "CC-BY-4.0" },
+                conjunction: "and",
+                right: { license: "Apache-2.0" },
+            },
+        },
+    );
+});
+
+it("allows mixed-case `and`, `or`, and `with`", function () {
+    var variants = [
+        "MIT and BSD-3-Clause or GPL-2.0-only with GCC-exception-2.0",
+        "MIT aNd BSD-3-Clause oR GPL-2.0-only wITh GCC-exception-2.0",
+        "MIT AnD BSD-3-Clause Or GPL-2.0-only WitH GCC-exception-2.0",
+    ];
+    var result = {
+        left: {
+            left: { license: "MIT" },
+            conjunction: "and",
+            right: { license: "BSD-3-Clause" },
+        },
+        conjunction: "or",
+        right: {
+            license: "GPL-2.0-only",
+            exception: "GCC-exception-2.0",
+        },
+    };
+    for (let index = 0; index < variants.length; index++) {
+        const variant = variants[index];
+        assert.deepStrictEqual(p(variant), result);
+    }
+});
+
+function it(message, test) {
+    test();
+}
