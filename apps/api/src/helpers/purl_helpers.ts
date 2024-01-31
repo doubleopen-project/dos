fef2f20b2a70aedea2b1d38a114f452966099790
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-01-31 11:24:15 +0200

    fix(api): Edit parsePurl qualifier processing
    Use encodeURIComponent function on all qualifier key value pair values. This fixes
    a 'Generated purl does not match the requested purl' issue.
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- apps/api/src/helpers/purl_helpers.ts
+++ apps/api/src/helpers/purl_helpers.ts
@@ -4,15 +4,6 @@
 
 import { PackageURL } from "packageurl-js";
 
-const isUrl = (str: string) => {
-    try {
-        new URL(str);
-        return true;
-    } catch (err) {
-        return false;
-    }
-};
-
 export const parsePurl = (purl: string) => {
     const parsedPurl = PackageURL.fromString(purl);
 
@@ -20,16 +11,10 @@ export const parsePurl = (purl: string) => {
 
     if (parsedPurl.qualifiers) {
         for (const [key, value] of Object.entries(parsedPurl.qualifiers)) {
-            let valueToSet = value;
-
-            if (isUrl(value)) {
-                valueToSet = encodeURIComponent(value);
-            }
-
             if (qualifiers) {
-                qualifiers += "&" + key + "=" + valueToSet;
+                qualifiers += "&" + key + "=" + encodeURIComponent(value);
             } else {
-                qualifiers = key + "=" + valueToSet;
+                qualifiers = key + "=" + encodeURIComponent(value);
             }
         }
     }
