bb62a227709bef5d9f9767ecbda9aa3189571940
Etsija <jyrki.keisala@gmail.com>
2024-02-05 15:17:49 +0200

    feat(ui): Add icons to tabs
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/components/ClearanceToolbar.tsx
+++ apps/clearance_ui/src/components/ClearanceToolbar.tsx
@@ -5,6 +5,9 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { LuFileStack, LuFolderTree } from "react-icons/lu";
+import { TbFoldersOff } from "react-icons/tb";
+import { TfiPencil } from "react-icons/tfi";
 import { cn } from "@/lib/utils";
 
 const ClearanceToolbar = () => {
@@ -12,18 +15,21 @@ const ClearanceToolbar = () => {
     const purl = router.query.purl as string;
     console.log(purl);
     return (
-        <div>
+        <div className="pl-2 pt-1">
             <Link
                 href={`/packages/${encodeURIComponent(purl)}`}
                 className={cn(
                     router.pathname === "/packages/[purl]" ||
                         router.pathname.includes("/tree/")
-                        ? "border-b-4 border-orange-600 font-semibold"
+                        ? "border-b-4 border-[#ff3366] font-semibold"
                         : "",
-                    "inline-block rounded-sm px-2 py-1 text-sm hover:bg-gray-100 hover:text-gray-900",
+                    "inline-block rounded-sm px-2 py-1 text-xs hover:bg-gray-100 hover:text-gray-900",
                 )}
             >
-                Inspect
+                <div className="flex items-center">
+                    <LuFolderTree className="mr-1" />
+                    Inspect
+                </div>
             </Link>
             <Link
                 href={`/packages/${encodeURIComponent(
@@ -31,34 +37,43 @@ const ClearanceToolbar = () => {
                 )}/license_conclusions`}
                 className={cn(
                     router.pathname === "/packages/[purl]/license_conclusions"
-                        ? "border-b-4 border-orange-600 font-semibold"
+                        ? "border-b-4 border-[#ff3366] font-semibold"
                         : "",
-                    "inline-block rounded-sm px-2 py-1 text-sm hover:bg-gray-100 hover:text-gray-900",
+                    "inline-block rounded-sm px-2 py-1 text-xs hover:bg-gray-100 hover:text-gray-900",
                 )}
             >
-                License
+                <div className="flex items-center">
+                    <TfiPencil className="mr-1" />
+                    License Conclusions
+                </div>
             </Link>
             <Link
                 href={`/packages/${encodeURIComponent(purl)}/bulk_conclusions`}
                 className={cn(
                     router.pathname === "/packages/[purl]/bulk_conclusions"
-                        ? "border-b-4 border-orange-600 font-semibold"
+                        ? "border-b-4 border-[#ff3366] font-semibold"
                         : "",
-                    "inline-block rounded-sm px-2 py-1 text-sm hover:bg-gray-100 hover:text-gray-900",
+                    "inline-block rounded-sm px-2 py-1 text-xs hover:bg-gray-100 hover:text-gray-900",
                 )}
             >
-                Bulk
+                <div className="flex items-center">
+                    <LuFileStack className="mr-1" />
+                    Bulk Conclusions
+                </div>
             </Link>
             <Link
                 href={`/packages/${encodeURIComponent(purl)}/path_exclusions`}
                 className={cn(
                     router.pathname === "/packages/[purl]/path_exclusions"
-                        ? "border-b-4 border-orange-600 font-semibold"
+                        ? "border-b-4 border-[#ff3366] font-semibold"
                         : "",
-                    "inline-block rounded-sm px-2 py-1 text-sm hover:bg-gray-100 hover:text-gray-900",
+                    "inline-block rounded-sm px-2 py-1 text-xs hover:bg-gray-100 hover:text-gray-900",
                 )}
             >
-                Path
+                <div className="flex items-center">
+                    <TbFoldersOff className="mr-1" />
+                    Path Exclusions
+                </div>
             </Link>
         </div>
     );
