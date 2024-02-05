44ccc3d723d57680d6798f984474b303ddaecb2b
Etsija <jyrki.keisala@gmail.com>
2024-02-05 12:12:15 +0200

    refactor(ui): Move ClearanceToolbar
    To ease up near-future refactoring of the folder structure, Move
    the clearance toolbar component to the root of components folder,
    instead of yet another subdirectory.
    
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/components/MainUI.tsx
+++ apps/clearance_ui/src/components/MainUI.tsx
@@ -9,7 +9,7 @@ import {
     ResizablePanel,
     ResizablePanelGroup,
 } from "@/components/ui/resizable";
-import ClearanceToolbar from "@/components/clearance_toolbar/ClearanceToolbar";
+import ClearanceToolbar from "@/components/ClearanceToolbar";
 import CodeInspector from "@/components/file_inspector/CodeInspector";
 import PackageTree from "@/components/package_inspector/PackageTree";
 import ClearanceInspector from "./clearance_inspector/ClearanceInspector";
