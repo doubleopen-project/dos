1879d00a6b67a882a0d054bab98d3d60d823cb37
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-02-06 11:01:45 +0200

    refactor(ui): Use useQueryState for path exclusions table
    Handle table state with useQueryState. Also use debounce with input field.
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- apps/clearance_ui/src/components/path_exclusion_table/PathExclusionList.tsx
+++ apps/clearance_ui/src/components/path_exclusion_table/PathExclusionList.tsx
@@ -4,6 +4,12 @@
 
 import React from "react";
 import { ZodiosResponseByPath } from "@zodios/core";
+import {
+    parseAsInteger,
+    parseAsString,
+    parseAsStringEnum,
+    useQueryState,
+} from "next-usequerystate";
 import { userAPI } from "validation-helpers";
 import { userHooks } from "@/hooks/zodiosHooks";
 import { columns } from "@/components/path_exclusion_table/columns";
@@ -14,9 +20,55 @@ type PathExclusionListProps = {
 };
 
 const PathExclusionList = ({ user }: PathExclusionListProps) => {
-    const { data, isLoading, error } = userHooks.useGetPathExclusions({
-        withCredentials: true,
-    });
+    const [pageSize] = useQueryState(
+        "pageSize",
+        parseAsInteger.withDefault(10),
+    );
+    const [pageIndex, setPageIndex] = useQueryState(
+        "pageIndex",
+        parseAsInteger.withDefault(1),
+    );
+    const [purl] = useQueryState("purl", parseAsString);
+
+    const [sortBy, setSortBy] = useQueryState(
+        "sortBy",
+        parseAsStringEnum([
+            "pkg",
+            "pattern",
+            "reason",
+            "comment",
+            "updatedAt",
+            "username",
+        ]).withDefault("updatedAt"),
+    );
+    const [sortOrder, setSortOrder] = useQueryState(
+        "sortOrder",
+        parseAsStringEnum(["asc", "desc"]).withDefault("desc"),
+    );
+
+    const peCntQuery = userHooks.useGetPathExclusionsCount(
+        {
+            withCredentials: true,
+            queries: {
+                purl: purl !== null ? purl : undefined,
+            },
+        },
+        { enabled: !!user },
+    );
+
+    const { data, isLoading, error } = userHooks.useGetPathExclusions(
+        {
+            withCredentials: true,
+            queries: {
+                pageIndex: pageIndex - 1,
+                pageSize,
+                sortBy: sortBy !== null ? sortBy : undefined,
+                sortOrder: sortOrder !== null ? sortOrder : undefined,
+                purl: purl !== null ? purl : undefined,
+            },
+        },
+        { enabled: !!user && !!pageSize && !!pageIndex },
+    );
     if (isLoading) {
         return (
             <div className="flex h-full items-center justify-center align-middle">
@@ -28,12 +80,27 @@ const PathExclusionList = ({ user }: PathExclusionListProps) => {
     if (!data) return <div>No data</div>;
 
     // Get user role, to decide what rights the user has for this view
-    const tableColumns = columns(user);
+    const tableColumns = columns(
+        user,
+        sortBy,
+        sortOrder,
+        setSortBy,
+        setSortOrder,
+        setPageIndex,
+    );
 
     return (
         <div className="container mx-auto">
             {user && (
-                <DataTable columns={tableColumns} data={data.pathExclusions} />
+                <DataTable
+                    columns={tableColumns}
+                    data={data.pathExclusions}
+                    pageCount={
+                        peCntQuery.data?.count
+                            ? Math.ceil(peCntQuery.data.count / pageSize)
+                            : 0
+                    }
+                />
             )}
         </div>
     );
