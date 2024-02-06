6ff38f62796034988ccd9548a09820dbedf24242
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-02-06 13:06:36 +0200

    refactor(ui): Use useQueryState for bulk conclusions table
    Handle table state with useQueryState. Also use debounce with input field.
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- apps/clearance_ui/src/components/bulk_conclusion_table/DataTable.tsx
+++ apps/clearance_ui/src/components/bulk_conclusion_table/DataTable.tsx
@@ -2,18 +2,19 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import {
     ColumnDef,
-    ColumnFiltersState,
     flexRender,
     getCoreRowModel,
-    getFilteredRowModel,
-    getPaginationRowModel,
-    getSortedRowModel,
-    SortingState,
     useReactTable,
 } from "@tanstack/react-table";
+import debounce from "lodash.debounce";
+import {
+    parseAsInteger,
+    parseAsString,
+    useQueryState,
+} from "next-usequerystate";
 import { Input } from "@/components/ui/input";
 import {
     Table,
@@ -23,33 +24,36 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
-import { DataTablePagination } from "@/components/package_table/DataTablePagination";
+import { DataTablePagination2 } from "@/components/package_table/DataTablePagination2";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
     data: TData[];
+    pageCount: number;
 }
 
 export function DataTable<TData, TValue>({
     columns,
     data,
+    pageCount,
 }: DataTableProps<TData, TValue>) {
-    const [sorting, setSorting] = useState<SortingState>([]);
-    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+    const [purl, setPurl] = useQueryState("purl", parseAsString);
+    // The setPageIndex cannot be recognized as a callable expression without the pageIndex, so it
+    // is added here and an eslint-disable-next-line is added to ignore the unused variable
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [pageIndex, setPageIndex] = useQueryState(
+        "pageIndex",
+        parseAsInteger.withDefault(1),
+    );
+    const [inputValue, setInputValue] = useState<string>(purl || "");
+    const debounceSetPurl = useMemo(() => debounce(setPurl, 300), [setPurl]);
 
     const table = useReactTable({
         data,
         columns,
+        pageCount: pageCount,
         getCoreRowModel: getCoreRowModel(),
-        onSortingChange: setSorting,
-        getSortedRowModel: getSortedRowModel(),
-        onColumnFiltersChange: setColumnFilters,
-        getFilteredRowModel: getFilteredRowModel(),
-        state: {
-            sorting,
-            columnFilters,
-        },
-        getPaginationRowModel: getPaginationRowModel(),
+        manualPagination: true,
     });
 
     return (
@@ -57,19 +61,20 @@ export function DataTable<TData, TValue>({
             <div className="flex items-center justify-between py-2">
                 <Input
                     placeholder="Search packages by purl"
-                    value={
-                        (table
-                            .getColumn("contextPurl")
-                            ?.getFilterValue() as string) ?? ""
-                    }
-                    onChange={(event) =>
-                        table
-                            .getColumn("contextPurl")
-                            ?.setFilterValue(event.target.value)
-                    }
+                    value={inputValue ?? ""}
+                    onChange={(event) => {
+                        setInputValue(event.target.value);
+                        if (event.target.value === "") {
+                            debounceSetPurl(null);
+                        } else {
+                            debounceSetPurl(event.target.value);
+                        }
+                        setPageIndex(1);
+                    }}
                     className="max-w-sm"
+                    autoFocus
                 />
-                <DataTablePagination table={table} />
+                <DataTablePagination2 table={table} />
             </div>
             <div className="rounded-md border">
                 <Table>
