781a33d300d8303c615f5947da21f11427c6430d
Etsija <jyrki.keisala@gmail.com>
2024-02-08 08:33:33 +0200

    feat(ui): Move links to under the menu button in navbar
    Move the links to Package Library and Clearance Library to the Sheet
    component that opens from the menu button.
    
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/components/navigation/Navbar.tsx
+++ apps/clearance_ui/src/components/navigation/Navbar.tsx
@@ -4,9 +4,20 @@
 
 import React from "react";
 import Link from "next/link";
+import { AiOutlineEye } from "react-icons/ai";
 import { IoMenuSharp } from "react-icons/io5";
+import { LuFileStack } from "react-icons/lu";
 import { useUser } from "@/hooks/useUser";
 import { Button } from "@/components/ui/button";
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
 import { ModeToggle } from "@/components/ModeToggle";
 import UserMenuItem from "./UserMenuItem";
 
@@ -15,30 +26,62 @@ const Navbar = () => {
     return (
         <div className="overflow-none flex flex-row items-center justify-between border-b-[1px] py-2">
             <div className="flex items-center justify-start">
-                <Button variant="outline" size="sm" className="ml-2">
-                    <IoMenuSharp size="20px" color="gray" />
-                </Button>
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="sm" className="ml-2">
+                            <IoMenuSharp size="20px" color="gray" />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent side="left">
+                        <SheetHeader>
+                            <SheetTitle>Pages</SheetTitle>
+                            <SheetDescription>
+                                Additional pages that help in the clearance
+                                work. This includes libraries, which list all
+                                the packages and clearances available in the
+                                database.
+                            </SheetDescription>
+                        </SheetHeader>
+                        {user && (
+                            <div className="grid gap-4 py-4">
+                                <SheetClose asChild>
+                                    <Link
+                                        href="/packages"
+                                        className="inline-block rounded-lg px-2 py-1 text-sm hover:bg-gray-100 hover:text-gray-900"
+                                    >
+                                        <div className="flex items-center">
+                                            <LuFileStack
+                                                size="20px"
+                                                className="mr-2"
+                                            />
+                                            Package Library
+                                        </div>
+                                    </Link>
+                                </SheetClose>
+                                <SheetClose asChild>
+                                    <Link
+                                        href="/clearances"
+                                        className="inline-block rounded-lg px-2 py-1 text-sm hover:bg-gray-100 hover:text-gray-900"
+                                    >
+                                        <div className="flex items-center">
+                                            <AiOutlineEye
+                                                size="20px"
+                                                className="mr-2"
+                                            />
+                                            Clearance Library
+                                        </div>
+                                    </Link>
+                                </SheetClose>
+                            </div>
+                        )}
+                    </SheetContent>
+                </Sheet>
                 <Link href="/" className="pl-2 pr-4">
                     <span className="logo">doubleOpen</span>
                     <span className="logo logo-brackets">()</span>
                 </Link>
-                {user && (
-                    <>
-                        <Link
-                            href="/packages"
-                            className="inline-block rounded-lg px-2 py-1 text-sm hover:bg-gray-100 hover:text-gray-900"
-                        >
-                            Package Library
-                        </Link>
-                        <Link
-                            href="/clearances"
-                            className="inline-block rounded-lg px-2 py-1 text-sm hover:bg-gray-100 hover:text-gray-900"
-                        >
-                            Clearance Library
-                        </Link>
-                    </>
-                )}
             </div>
+            {/* Insert package and file path here */}
             <div>
                 <UserMenuItem user={user} className="mr-1" />
                 <ModeToggle className="mr-1" />
