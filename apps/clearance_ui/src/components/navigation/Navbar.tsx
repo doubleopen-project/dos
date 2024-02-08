3ed53237a3e87afcc417948ffe634ef430695ce9
Etsija <jyrki.keisala@gmail.com>
2024-02-08 08:14:27 +0200

    feat(ui): Add navigation button to navbar
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/components/navigation/Navbar.tsx
+++ apps/clearance_ui/src/components/navigation/Navbar.tsx
@@ -4,7 +4,9 @@
 
 import React from "react";
 import Link from "next/link";
+import { IoMenuSharp } from "react-icons/io5";
 import { useUser } from "@/hooks/useUser";
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ModeToggle";
 import UserMenuItem from "./UserMenuItem";
 
@@ -13,7 +15,10 @@ const Navbar = () => {
     return (
         <div className="overflow-none flex flex-row items-center justify-between border-b-[1px] py-2">
             <div className="flex items-center justify-start">
-                <Link href="/" className="px-4">
+                <Button variant="outline" size="sm" className="ml-2">
+                    <IoMenuSharp size="20px" color="gray" />
+                </Button>
+                <Link href="/" className="pl-2 pr-4">
                     <span className="logo">doubleOpen</span>
                     <span className="logo logo-brackets">()</span>
                 </Link>
