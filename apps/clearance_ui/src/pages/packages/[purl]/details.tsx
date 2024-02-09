a1556b72887de9f703859ebd9a3ad703105dff87
Etsija <jyrki.keisala@gmail.com>
2024-02-09 08:56:27 +0200

    feat(ui): Show basic package info
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/pages/packages/[purl]/details.tsx
+++ apps/clearance_ui/src/pages/packages/[purl]/details.tsx
@@ -4,42 +4,79 @@
 
 import React from "react";
 import { useRouter } from "next/router";
-import { Label } from "@/components/ui/label";
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableRow,
-} from "@/components/ui/table";
+import { PackageURL } from "packageurl-js";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import ClearanceToolbar from "@/components/ClearanceToolbar";
+import { parsePurlAndQualifiers } from "@/helpers/parsePurlAndQualifiers";
 
 const Details = () => {
     const router = useRouter();
     const purl = router.query.purl as string;
+    const parsedPurl = parsePurlAndQualifiers(purl);
+    const mainPurl = new PackageURL(
+        parsedPurl.type,
+        parsedPurl.namespace,
+        parsedPurl.name,
+        parsedPurl.version,
+        null,
+        null,
+    ).toString();
     return (
         <div className="flex h-full flex-col">
             <ClearanceToolbar />
             <div className="flex-1 border p-2">
-                <Label className="font-semibold">Package Information</Label>
-                <div className="rounded-lg border p-2">
-                    <Table>
-                        <TableBody>
+                <h3 className="font-semibold">Package Information</h3>
+
+                <Table className="mt-2 w-fit border p-2">
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="text-right font-semibold">
+                                Name:
+                            </TableCell>
+                            <TableCell>{parsedPurl.name}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="text-right font-semibold">
+                                Version:
+                            </TableCell>
+                            <TableCell>{parsedPurl.version}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="text-right font-semibold">
+                                Type:
+                            </TableCell>
+                            <TableCell>{parsedPurl.type}</TableCell>
+                        </TableRow>
+                        {parsedPurl.namespace && (
                             <TableRow>
-                                <TableCell className="font-semibold">
-                                    Name
+                                <TableCell className="text-right font-semibold">
+                                    Namespace:
                                 </TableCell>
-                                <TableCell>dos-monorepo</TableCell>
+                                <TableCell>{parsedPurl.namespace}</TableCell>
                             </TableRow>
+                        )}
+                        {parsedPurl.subpath && (
                             <TableRow>
-                                <TableCell className="font-semibold">
-                                    Version
+                                <TableCell className="text-right font-semibold">
+                                    Subpath:
                                 </TableCell>
-                                <TableCell>0.0.0</TableCell>
+                                <TableCell>{parsedPurl.subpath}</TableCell>
                             </TableRow>
-                        </TableBody>
-                    </Table>
-                </div>
+                        )}
+                        <TableRow>
+                            <TableCell className="text-right font-semibold">
+                                Clean PURL:
+                            </TableCell>
+                            <TableCell>{mainPurl}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="text-right font-semibold">
+                                Full PURL:
+                            </TableCell>
+                            <TableCell>{purl}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </div>
         </div>
     );
