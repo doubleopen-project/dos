98e81cac36132ecd5f28af2b1efb9ea2780ea0eb
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-02-16 11:33:35 +0200

    refactor(db): Add updatedAt index for some tables
    Add index for updatedAt field on LicenseConclusion, BulkConclusion, PathExclusion, and
    Package tables to enhance performance, as the queries fetching them are now ordered by
    updatedAt by default.
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

---
+++ packages/database/prisma/migrations/20240216115443_add_updated_at_index_for_some_tables/migration.sql
@@ -0,0 +1,11 @@
+-- CreateIndex
+CREATE INDEX "BulkConclusion_updatedAt_idx" ON "BulkConclusion"("updatedAt");
+
+-- CreateIndex
+CREATE INDEX "LicenseConclusion_updatedAt_idx" ON "LicenseConclusion"("updatedAt");
+
+-- CreateIndex
+CREATE INDEX "Package_updatedAt_idx" ON "Package"("updatedAt");
+
+-- CreateIndex
+CREATE INDEX "PathExclusion_updatedAt_idx" ON "PathExclusion"("updatedAt");
