95f42f3d6d4cd3e9db4bda2f082a85fe11278631
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-02-19 15:03:56 +0200

    feat(api): Add function for extracting a string from glob
    This function will be used to get a string from a glob to be used in database
    queries, so that the amount of filetree paths to compare with the glob will be
    lower.
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

---
+++ apps/api/src/helpers/globHelpers.ts
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2024 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+import path from "path";
+import globParent from "glob-parent";
+import isValid from "is-valid-path";
+
+// This function is used to extract a string from a glob, to be used in database
+// queries, to reduce the amount of filetree paths that will be compared with the glob.
+export const extractStringFromGlob = (glob: string) => {
+    const nonMagicParentPath = globParent(glob);
+    // Case where the parent path is not magic
+    if (nonMagicParentPath !== ".") return nonMagicParentPath;
+    else {
+        const globParts = glob.split("/").filter((part) => isValid(part));
+        // Case where the parent path is magic, but there are non-magic parts in the glob
+        if (globParts.length > 0) return globParts[0];
+        else {
+            // Case where the parent path is magic and all the parts of the glob are magic
+            // but an extension is present
+            if (path.extname(glob) !== "") return path.extname(glob);
+            // Case where the parent path is magic and all the parts of the glob are magic
+            else return undefined;
+        }
+    }
+};
