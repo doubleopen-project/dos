9b5087537a6ce89b5c139a99914c0d1c28f0af99
Etsija <jyrki.keisala@gmail.com>
2024-02-19 10:39:10 +0200

    feat(ui): Unify styling of tabs
    Make the bulk conclusion and path exclusion components use a similar
    styling and layout as the license conclusion component.
    
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/components/main_ui/package_bulk_conclusions/BulkConclusion.tsx
+++ apps/clearance_ui/src/components/main_ui/package_bulk_conclusions/BulkConclusion.tsx
@@ -53,49 +53,55 @@ const BulkConclusion = ({
             <div className="mr-1 flex-1 items-start text-left">
                 <div className="flex w-full flex-col gap-1">
                     <div className="flex items-center justify-between text-sm">
-                        <span className="font-semibold">
+                        <div className="font-semibold">
                             {
                                 new Date(bulkConclusion.updatedAt)
                                     .toISOString()
                                     .split("T")[0]
                             }
-                        </span>
-                        <span className="rounded-sm bg-orange-400 p-1 text-xs font-semibold">
+                        </div>
+                        <div className="rounded-sm bg-orange-400 p-1 text-xs font-semibold">
                             {bulkConclusion.user.username}
-                        </span>
+                        </div>
                     </div>
-                    <div className="text-xs">
-                        <span className="mr-1 font-semibold">Concluded:</span>
-                        <span className="rounded-sm bg-green-400 dark:text-black">
+                    <div className="flex text-xs">
+                        <div className="mr-2 flex whitespace-nowrap font-semibold">
+                            Concluded:
+                        </div>
+                        <div className="rounded-sm bg-green-400 dark:text-black">
                             {bulkConclusion.concludedLicenseExpressionSPDX}
-                        </span>
+                        </div>
                     </div>
-                    <div className="text-xs">
-                        <span className="mr-1 font-semibold">Pattern:</span>
-                        <span className="break-all">
+                    <div className="flex text-xs">
+                        <div className="mr-2 flex whitespace-nowrap font-semibold">
+                            Pattern:
+                        </div>
+                        <div className="break-all">
                             {bulkConclusion.pattern}
-                        </span>
+                        </div>
                     </div>
-                    <div className="text-muted-foreground text-xs">
-                        <span className="mr-1 font-semibold">Comment:</span>
-                        <span className="italic">{bulkConclusion.comment}</span>
+                    <div className="text-muted-foreground flex text-xs">
+                        <div className="mr-2 flex whitespace-nowrap font-semibold">
+                            Comment:
+                        </div>
+                        <div className="italic">{bulkConclusion.comment}</div>
                     </div>
-                    <div className="text-xs">
-                        <span className="mr-1 font-semibold">
+                    <div className="flex text-xs">
+                        <div className="mr-2 flex whitespace-nowrap font-semibold">
                             Files affected in this package:
-                        </span>
-                        <span>
+                        </div>
+                        <div>
                             <Badge className="bg-blue-400 p-0.5 font-bold">
                                 {pathsMatching.length}
                             </Badge>
-                        </span>
+                        </div>
                     </div>
                     {bulkConclusion.local && (
-                        <span>
+                        <div>
                             <Badge className="bg-red-400 p-0.5 font-bold">
                                 LOCAL
                             </Badge>
-                        </span>
+                        </div>
                     )}
                 </div>
             </div>
