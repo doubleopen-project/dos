e2d34c9a5831be419a646fc90df399c5601b9345
Etsija <jyrki.keisala@gmail.com>
2024-02-19 11:08:24 +0200

    test(ui): Add a test for license conclusions
    Add a test for creating a license conclusion in the Main UI, editing
    and deleting it in the license conclusions tab of the clearance toolbar.
    
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/tests/e2e/logged-as-test-user/license-conclusion.spec.ts
+++ apps/clearance_ui/tests/e2e/logged-as-test-user/license-conclusion.spec.ts
@@ -382,3 +382,88 @@ test("create license conclusion without comment, edit license, and finally delet
     await toastDeleted.waitFor({ state: "hidden" });
     console.log("license conclusion deleted from Clearance Library");
 });
+
+test("create license conclusion, edit, and delete from clearance toolbar", async ({
+    page,
+}) => {
+    const license = "GPL-3.0-or-later";
+    const comment =
+        "Test create license conclusion, edit, and delete from clearance toolbar";
+    const editedLicense = "GPL-3.0-only";
+    const editedComment = "Edited test comment";
+
+    // Create a license conclusion
+    await page.getByText("apps").click();
+    await page.getByText("curation_front_end").click();
+    await page.getByRole("link", { name: "next.config.js" }).click();
+    await page.getByPlaceholder("Write your SPDX expression").click();
+    await page.getByPlaceholder("Write your SPDX expression").fill(license);
+    await page
+        .getByPlaceholder("Comment on your license conclusion...")
+        .click();
+    await page
+        .getByPlaceholder("Comment on your license conclusion...")
+        .fill(comment);
+    page.once("dialog", (dialog) => {
+        dialog.accept().catch(() => {});
+    });
+    await page.getByRole("button", { name: "Submit" }).click();
+    // Wait for the toast to appear, contain a success text and disappear
+    const toastCreated = page.getByRole("status").first();
+    await toastCreated.waitFor({ state: "visible" });
+    await expect(toastCreated).toContainText(
+        "License conclusion added successfully.",
+    );
+    await toastCreated.waitFor({ state: "hidden" });
+    console.log("license conclusion created");
+
+    // Edit the license conclusion
+    await page.getByRole("link", { name: "License Conclusions" }).click();
+    await page
+        .getByTestId("license-conclusion")
+        .filter({ hasText: comment })
+        .getByRole("button", { name: "edit" })
+        .click();
+    await page.getByRole("textbox", { name: "spdx" }).click();
+    await page.getByRole("textbox", { name: "spdx" }).fill(editedLicense);
+    await page.getByLabel("Comment").click();
+    await page.getByLabel("Comment").fill(editedComment);
+    await page.getByTestId("license-conclusion-edit").click();
+    // Wait for the toast to appear, contain a success text and disappear
+    const toastEdited = page.getByRole("status").first();
+    await toastEdited.waitFor({ state: "visible" });
+    await expect(toastEdited).toContainText(
+        "License conclusion edited and saved successfully.",
+    );
+    await toastEdited.waitFor({ state: "hidden" });
+    console.log("license conclusion edited in clearance toolbar");
+
+    // Delete the same bulk conclusion
+    await expect(
+        page
+            .getByTestId("license-conclusion")
+            .filter({ hasText: editedComment }),
+    ).toContainText(editedLicense);
+    await expect(
+        page
+            .getByTestId("license-conclusion")
+            .filter({ hasText: editedComment }),
+    ).toContainText(editedComment);
+    await page
+        .getByTestId("license-conclusion")
+        .filter({ hasText: editedComment })
+        .getByTestId("delete-clearance-button")
+        .click();
+    await expect(
+        page.getByLabel("Delete").getByText(editedLicense),
+    ).toContainText(editedLicense);
+    await page.getByRole("button", { name: "Delete" }).click();
+    // Wait for the toast to appear, contain a success text and disappear
+    const toastDeleted = page.getByRole("status").first();
+    await toastDeleted.waitFor({ state: "visible" });
+    await expect(toastDeleted).toContainText(
+        "License conclusion deleted successfully.",
+    );
+    await toastDeleted.waitFor({ state: "hidden" });
+    console.log("license conclusion deleted from clearance toolbar");
+});
