4c6f3b7c0c7a7e36a5ac9b0f5ca22fc37600c0a4
Etsija <jyrki.keisala@gmail.com>
2024-02-19 12:09:23 +0200

    refactor(ui): Move left panel components
    Move the components implementing the left panel of the Main UI to
    main_ui/inspector/package_inspector directory.  Also rename PackageTree
    to PackageInspector, to better reflect the naming scheme used in the
    Main UI.
    
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/components/main_ui/inspector/package_inspector/Node.tsx
+++ apps/clearance_ui/src/components/main_ui/inspector/package_inspector/Node.tsx
@@ -11,7 +11,7 @@ import {
     BsFolder2Open as FolderOpen,
 } from "react-icons/bs";
 import { MdArrowDropDown, MdArrowRight } from "react-icons/md";
-import LicenseHitCircle from "@/components/package_inspector/LicenseHitCircle";
+import LicenseHitCircle from "@/components/main_ui/inspector/package_inspector/LicenseHitCircle";
 import { cn } from "@/lib/utils";
 import type { TreeNode } from "@/types/index";
 
