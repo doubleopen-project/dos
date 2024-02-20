c7fc11fe30c96061045a31337378f0aab8e57e26
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-02-20 13:27:42 +0200

    test(ui): Refactor test set that still uses chai
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- apps/clearance_ui/tests/unit/parsePurlTS.test.ts
+++ apps/clearance_ui/tests/unit/parsePurlTS.test.ts
@@ -4,7 +4,6 @@
 
 // Unit tests for parsePurlAndQualifiers helper function
 
-import { assert } from "chai";
 import { parsePurlAndQualifiers } from "@/helpers/parsePurlAndQualifiers";
 
 describe("parsePurlAndQualifiers tests", () => {
@@ -13,17 +12,14 @@ describe("parsePurlAndQualifiers tests", () => {
             "pkg:npm/%40floating-ui/core@1.5.2?vcs_type=Git&vcs_url=https%3A%2F%2Fgithub.com%2Ffloating-ui%2Ffloating-ui.git&vcs_revision=78e8d87ec2fd2172921937e24390806d7dedb636&resolved_revision=78e8d87ec2fd2172921937e24390806d7dedb636#packages/core";
         const parsedPurl = parsePurlAndQualifiers(purl);
 
-        assert.strictEqual(parsedPurl.qualifiers?.vcs_type, "Git");
-        assert.strictEqual(
-            parsedPurl.qualifiers?.vcs_url,
+        expect(parsedPurl.qualifiers?.vcs_type).toBe("Git");
+        expect(parsedPurl.qualifiers?.vcs_url).toBe(
             "https://github.com/floating-ui/floating-ui.git",
         );
-        assert.strictEqual(
-            parsedPurl.qualifiers?.vcs_revision,
+        expect(parsedPurl.qualifiers?.vcs_revision).toBe(
             "78e8d87ec2fd2172921937e24390806d7dedb636",
         );
-        assert.strictEqual(
-            parsedPurl.qualifiers?.resolved_revision,
+        expect(parsedPurl.qualifiers?.resolved_revision).toBe(
             "78e8d87ec2fd2172921937e24390806d7dedb636",
         );
     });
@@ -33,12 +29,10 @@ describe("parsePurlAndQualifiers tests", () => {
             "pkg:npm/%40radix-ui/react-context@1.0.0?download_url=https%3A%2F%2Fregistry.npmjs.org%2F%40radix-ui%2Freact-context%2F-%2Freact-context-1.0.0.tgz&checksum=sha1%3Af38e30c5859a9fb5e9aa9a9da452ee3ed9e0aee0";
         const parsedPurl = parsePurlAndQualifiers(purl);
 
-        assert.strictEqual(
-            parsedPurl.qualifiers?.download_url,
+        expect(parsedPurl.qualifiers?.download_url).toBe(
             "https://registry.npmjs.org/@radix-ui/react-context/-/react-context-1.0.0.tgz",
         );
-        assert.strictEqual(
-            parsedPurl.qualifiers?.checksum,
+        expect(parsedPurl.qualifiers?.checksum).toBe(
             "sha1:f38e30c5859a9fb5e9aa9a9da452ee3ed9e0aee0",
         );
     });
@@ -48,22 +42,19 @@ describe("parsePurlAndQualifiers tests", () => {
             "pkg:npm/%40floating-ui/core@1.5.2?vcs_type=Git&vcs_url=https%3A%2F%2Fgithub.com%2Ffloating-ui%2Ffloating-ui.git&vcs_revision=78e8d87ec2fd2172921937e24390806d7dedb636&resolved_revision=78e8d87ec2fd2172921937e24390806d7dedb636#packages/core";
         const parsedPurl = parsePurlAndQualifiers(purl);
 
-        assert.strictEqual(parsedPurl.type, "npm");
-        assert.strictEqual(parsedPurl.namespace, "@floating-ui");
-        assert.strictEqual(parsedPurl.name, "core");
-        assert.strictEqual(parsedPurl.version, "1.5.2");
-        assert.strictEqual(parsedPurl.subpath, "packages/core");
-        assert.strictEqual(parsedPurl.qualifiers?.vcs_type, "Git");
-        assert.strictEqual(
-            parsedPurl.qualifiers?.vcs_url,
+        expect(parsedPurl.type).toBe("npm");
+        expect(parsedPurl.namespace).toBe("@floating-ui");
+        expect(parsedPurl.name).toBe("core");
+        expect(parsedPurl.version).toBe("1.5.2");
+        expect(parsedPurl.subpath).toBe("packages/core");
+        expect(parsedPurl.qualifiers?.vcs_type).toBe("Git");
+        expect(parsedPurl.qualifiers?.vcs_url).toBe(
             "https://github.com/floating-ui/floating-ui.git",
         );
-        assert.strictEqual(
-            parsedPurl.qualifiers?.vcs_revision,
+        expect(parsedPurl.qualifiers?.vcs_revision).toBe(
             "78e8d87ec2fd2172921937e24390806d7dedb636",
         );
-        assert.strictEqual(
-            parsedPurl.qualifiers?.resolved_revision,
+        expect(parsedPurl.qualifiers?.resolved_revision).toBe(
             "78e8d87ec2fd2172921937e24390806d7dedb636",
         );
     });
@@ -73,17 +64,15 @@ describe("parsePurlAndQualifiers tests", () => {
             "pkg:npm/%40radix-ui/react-context@1.0.0?download_url=https%3A%2F%2Fregistry.npmjs.org%2F%40radix-ui%2Freact-context%2F-%2Freact-context-1.0.0.tgz&checksum=sha1%3Af38e30c5859a9fb5e9aa9a9da452ee3ed9e0aee0";
         const parsedPurl = parsePurlAndQualifiers(purl);
 
-        assert.strictEqual(parsedPurl.type, "npm");
-        assert.strictEqual(parsedPurl.namespace, "@radix-ui");
-        assert.strictEqual(parsedPurl.name, "react-context");
-        assert.strictEqual(parsedPurl.version, "1.0.0");
-        assert.strictEqual(parsedPurl.subpath, null);
-        assert.strictEqual(
-            parsedPurl.qualifiers?.download_url,
+        expect(parsedPurl.type).toBe("npm");
+        expect(parsedPurl.namespace).toBe("@radix-ui");
+        expect(parsedPurl.name).toBe("react-context");
+        expect(parsedPurl.version).toBe("1.0.0");
+        expect(parsedPurl.subpath).toBe(null);
+        expect(parsedPurl.qualifiers?.download_url).toBe(
             "https://registry.npmjs.org/@radix-ui/react-context/-/react-context-1.0.0.tgz",
         );
-        assert.strictEqual(
-            parsedPurl.qualifiers?.checksum,
+        expect(parsedPurl.qualifiers?.checksum).toBe(
             "sha1:f38e30c5859a9fb5e9aa9a9da452ee3ed9e0aee0",
         );
     });
