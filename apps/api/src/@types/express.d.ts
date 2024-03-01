19bea2bd1d5f5dadfbba56e93b3b3cb1505bcc11
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-03-01 11:39:35 +0200

    feat(api): Add keycloak config
    This configuration will be taken into use when swithcing to using Keycloak instead of Passport.js
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

---
+++ apps/api/src/@types/express.d.ts
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: 2024 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+declare namespace Express {
+    export interface Request {
+        hasRequiredRoles: boolean;
+        kauth: {
+            grant: {
+                access_token: {
+                    token: string;
+                    clientId: string;
+                    header: {
+                        alg: string;
+                        typ: string;
+                        kid: string;
+                    };
+                    content: {
+                        exp: number;
+                        iat: number;
+                        jti: string;
+                        iss: string;
+                        aud: string;
+                        sub: string;
+                        typ: string;
+                        azp: string;
+                        session_state: string;
+                        acr: string;
+                        "allowed-origins": string[];
+                        realm_access: {
+                            roles: string[];
+                        };
+                        resource_access: {
+                            [process.env.KEYCLOAK_CLIENT_ID]: {
+                                roles: string[];
+                            };
+                            account: {
+                                roles: string[];
+                            };
+                        };
+                        scope: string;
+                        sid: string;
+                        email_verified: boolean;
+                        preferred_username: string;
+                    };
+                    signature: Buffer | unknown;
+                    signed: string;
+                };
+            };
+        };
+    }
+}
---
+++ apps/api/src/config/keycloak.ts
@@ -0,0 +1,92 @@
+// SPDX-FileCopyrightText: 2024 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+import genFunc from "connect-pg-simple";
+import type { Request, Response } from "express";
+import session from "express-session";
+import Keycloak, {
+    KeycloakConfig,
+    Keycloak as KeycloakType,
+} from "keycloak-connect";
+
+let _keycloak: KeycloakType;
+
+const PostgresqlStore = genFunc(session);
+
+const memoryStore = new PostgresqlStore({
+    conString: process.env.DATABASE_URL,
+    tableName: "Session",
+});
+
+const keycloakConfig: KeycloakConfig = {
+    realm: process.env.KEYCLOAK_REALM as string,
+    resource: process.env.KEYCLOAK_CLIENT_ID as string,
+    "auth-server-url": process.env.KEYCLOAK_URL as string,
+    "bearer-only": true,
+    "confidential-port": 0,
+    "ssl-required": "external",
+};
+
+Keycloak.prototype.accessDenied = async (
+    request: Request,
+    response: Response,
+) => {
+    console.log(request.headers.authorization);
+    if (!request.headers.authorization) {
+        return response.status(401).json({
+            errorCode: "token.empty",
+            message: "Token is missing.",
+        });
+    }
+
+    if (!request.kauth?.grant) {
+        return response.status(401).json({
+            errorCode: "token.invalid",
+            message:
+                "You are not authorized to access this resource, please check the sent token and make sure it is a valid token.",
+        });
+    }
+
+    _keycloak.storeGrant(
+        request.kauth.grant as unknown as Keycloak.Grant,
+        request,
+        response,
+    );
+
+    const getGrant = await _keycloak.getGrant(request, response);
+
+    if (getGrant.isExpired()) {
+        return response.status(401).json({
+            errorCode: "token.expired",
+            message: "Expired token, please enter a valid token to continue.",
+        });
+    }
+
+    if (!request.hasRequiredRoles) {
+        return response.status(403).json({
+            errorCode: "roles.required",
+            message:
+                "You do not have the required roles to access this resource.",
+        });
+    }
+
+    return response.status(401).json({
+        errorCode: "token.invalid",
+        message: "Invalid token, enter a new token.",
+    });
+};
+
+function getKeycloak(): KeycloakType {
+    if (!_keycloak) {
+        console.log("\nInitializing Keycloak...\n");
+
+        _keycloak = new Keycloak({ store: memoryStore }, keycloakConfig);
+
+        return _keycloak;
+    }
+
+    return _keycloak;
+}
+
+export { getKeycloak, memoryStore };
