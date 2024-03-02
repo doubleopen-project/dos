e4d237b488488570d3ecf6b5b016c226953f4d9e
Etsija <jyrki.keisala@gmail.com>
2024-03-02 15:47:59 +0200

    refactor(ui): Move some helper functions
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/helpers/treeSelectionUtils.ts
+++ apps/clearance_ui/src/helpers/treeSelectionUtils.ts
@@ -78,7 +78,7 @@ export const updateSelectedNodes = (
     // Recursively toggle selection of the node and its immediate descendants
     toggleSelectionRecursive(node, isSelected);
 
-    // Check if all children of the parent node are selected or deselected,
+    // Check how many of the parent node are selected or deselected,
     // and update the parent's selection status accordingly
     const parentNode = node.parent;
     if (parentNode) {
@@ -90,6 +90,7 @@ export const updateSelectedNodes = (
                 .includes(child.data.path);
         });
         const selectedChildrenCount = selectedChildren.length;
+        console.log("selectedChildrenCount: ", selectedChildrenCount);
         if (selectedChildrenCount === 0) {
             // Remove the parent node from the updatedNodes list if it has no children selected
             parentNode.data.selectionStatus = 0;
@@ -130,165 +131,6 @@ export const clearSelectedNodes = (selectedNodes: NodeApi<TreeNode>[]) => {
 
 // Function to create a simplified glob pattern from the selected nodes
 export const createGlob = (selectedNodes: NodeApi<TreeNode>[]) => {
-    // Helper function that takes an array of file paths and groups them
-    // into a single glob pattern
-    function generateGlobPattern(paths: string[]): string {
-        const patterns: string[] = [];
-
-        // Group paths by directory
-        const groupedPaths: { [key: string]: string[] } = {};
-        paths.forEach((path) => {
-            const directory =
-                path.lastIndexOf("/") === -1
-                    ? ""
-                    : path.substring(0, path.lastIndexOf("/") + 1);
-            const filename = path.substring(directory.length);
-            if (!groupedPaths[directory]) {
-                groupedPaths[directory] = [];
-            }
-            groupedPaths[directory].push(filename);
-        });
-
-        // Construct glob pattern for each directory
-        for (const directory in groupedPaths) {
-            const files = groupedPaths[directory];
-            let pattern;
-            // Handle root directory separately
-            if (directory === "") {
-                pattern =
-                    files.length === 1 ? files[0] : `{${files.join(",")}}`;
-            } else {
-                // Construct pattern based on the number of selected files
-                pattern =
-                    files.length === 1
-                        ? `${directory}${files[0]}`
-                        : `${directory}{${files.join(",")}}`;
-            }
-            patterns.push(pattern);
-        }
-
-        // Join the patterns with ',' to create a single glob pattern
-        let globPattern = patterns.join(",");
-
-        // Surround the pattern with curly braces if it contains multiple patterns
-        if (patterns.length > 1) {
-            globPattern = `{${globPattern}}`;
-        }
-
-        return globPattern;
-    }
-
-    // Helper function that takes in an array of nodes and a glob pattern,
-    // and returns an array of nodes that don't match the glob pattern.
-    function filterNodesByGlob(
-        nodes: NodeApi<TreeNode>[],
-        globPattern: string,
-    ): NodeApi<TreeNode>[] {
-        return nodes.filter((node) => {
-            return !minimatch(node.data.path ?? "", globPattern, { dot: true });
-        });
-    }
-
-    // Helper function that takes an internal node (directory) and returns
-    // all its descendants that have a selectionStatus of 1 (selected)
-    function getDescendants(node: NodeApi<TreeNode>): NodeApi<TreeNode>[] {
-        const descendants: NodeApi<TreeNode>[] = [];
-        if (node.children) {
-            node.children.forEach((child) => {
-                if (child.data.selectionStatus === 1) {
-                    descendants.push(child);
-                } else {
-                    descendants.push(...getDescendants(child));
-                }
-            });
-        }
-        return descendants;
-    }
-
-    // Helper function that takes an internal node (directory) and if its selectionStatus
-    // is 1 (selected), removes it and all its descendants from the selectedNodes list
-    function removeNodeAndDescendants(
-        node: NodeApi<TreeNode>,
-        nodes: NodeApi<TreeNode>[],
-    ) {
-        if (node.data.selectionStatus === 1) {
-            nodes = nodes.filter((n) => {
-                return n !== node && !getDescendants(node).includes(n);
-            });
-        }
-        return nodes;
-    }
-
-    // Helper function to remove lower-level paths from a string[] of paths
-    // Example: if the input is ["a/b/**", "a/**"], the output will be ["a/**"]
-    function removeInnerSubstitutions(paths: string[]): string[] {
-        return paths.filter(
-            (path, _, self) =>
-                !self.some(
-                    (otherPath) =>
-                        otherPath !== path && isPathSubset(otherPath, path),
-                ),
-        );
-    }
-
-    // Helper function to check if path1 is a subset of path2
-    function isPathSubset(path1: string, path2: string): boolean {
-        // Remove "/**" from the paths before splitting them into parts
-        const parts1 = path1.replace("/**", "").split("/");
-        const parts2 = path2.replace("/**", "").split("/");
-        return parts1.every((part, index) => part === parts2[index]);
-    }
-
-    // Helper function which takes in an array of nodes
-    // and groups the nodes by their parent directory
-    function groupNodesByDirectory(nodes: NodeApi<TreeNode>[]): {
-        [dir: string]: NodeApi<TreeNode>[];
-    } {
-        const groupedNodes: { [dir: string]: NodeApi<TreeNode>[] } = {};
-        nodes.forEach((node) => {
-            const directory =
-                node.data.path?.lastIndexOf("/") === -1
-                    ? ""
-                    : node.data.path?.substring(
-                          0,
-                          node.data.path.lastIndexOf("/") + 1,
-                      );
-            if (directory && !groupedNodes[directory]) {
-                groupedNodes[directory] = [];
-            }
-            if (directory) groupedNodes[directory].push(node);
-        });
-        return groupedNodes;
-    }
-
-    // Helper function which returns true, if the directory the node is in
-    // contains both files (isLeaf) and subdirectories (isInternal).
-    function hasFilesAndDirs(node: NodeApi<TreeNode>): boolean {
-        const parent = node.parent;
-        const children = parent?.children;
-        const nFiles = children?.filter((node) => node.isLeaf).length;
-        const nDirs = children?.filter((node) => node.isInternal).length;
-        if (nFiles && nFiles > 0 && nDirs && nDirs > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Helper function that takes in a node and returns all its
-    // siblings, ie. nodes that share the same parent and are leafs (isLeaf)
-    function getSiblings(node: NodeApi<TreeNode>): NodeApi<TreeNode>[] {
-        const parent = node.parent;
-        if (parent) {
-            return parent.children?.filter((child) => child.isLeaf) || [];
-        }
-        return [];
-    }
-
-    /******************************************************
-     ** Main part
-     ******************************************************/
-
     let remainingNodes = selectedNodes;
     let pattern: string[] = [];
 
@@ -325,7 +167,7 @@ export const createGlob = (selectedNodes: NodeApi<TreeNode>[]) => {
         const firstNode = groupedNodes[dir][0];
         const parent = firstNode.parent;
         if (hasFilesAndDirs(firstNode)) {
-            const siblings = getSiblings(firstNode);
+            const siblings = getFileSiblings(firstNode);
             if (groupedNodes[dir].length === siblings.length) {
                 // Push to glob only if the pattern is not already included
                 if (parent && !pattern.includes(parent.data.path + "/*")) {
@@ -346,3 +188,176 @@ export const createGlob = (selectedNodes: NodeApi<TreeNode>[]) => {
     console.log("final glob pattern: ", globPattern);
     return globPattern;
 };
+
+/**
+ * Helper functions
+ */
+
+// Helper function that returns the number of selected children of a node
+// Returned value is a string: "all", "some" or "none"
+export const getSelectedChildrenStatus = (node: NodeApi<TreeNode>) => {
+    const selectedChildren = node.children?.filter(
+        (child) => child.data.selectionStatus === 1,
+    );
+    if (selectedChildren?.length === 0) {
+        return "none";
+    } else if (selectedChildren?.length === node.children?.length) {
+        return "all";
+    } else {
+        return "some";
+    }
+};
+
+// Helper function that takes in an array of nodes and a glob pattern,
+// and returns an array of nodes that don't match the glob pattern.
+function filterNodesByGlob(
+    nodes: NodeApi<TreeNode>[],
+    globPattern: string,
+): NodeApi<TreeNode>[] {
+    return nodes.filter((node) => {
+        return !minimatch(node.data.path ?? "", globPattern, { dot: true });
+    });
+}
+
+// Helper function that takes in a node and returns all its
+// siblings, ie. nodes that share the same parent and are leafs (isLeaf)
+function getFileSiblings(node: NodeApi<TreeNode>): NodeApi<TreeNode>[] {
+    const parent = node.parent;
+    if (parent) {
+        return parent.children?.filter((child) => child.isLeaf) || [];
+    }
+    return [];
+}
+
+// Helper function which takes in an array of nodes
+// and groups the nodes by their parent directory
+function groupNodesByDirectory(nodes: NodeApi<TreeNode>[]): {
+    [dir: string]: NodeApi<TreeNode>[];
+} {
+    const groupedNodes: { [dir: string]: NodeApi<TreeNode>[] } = {};
+    nodes.forEach((node) => {
+        const directory =
+            node.data.path?.lastIndexOf("/") === -1
+                ? ""
+                : node.data.path?.substring(
+                      0,
+                      node.data.path.lastIndexOf("/") + 1,
+                  );
+        if (directory && !groupedNodes[directory]) {
+            groupedNodes[directory] = [];
+        }
+        if (directory) groupedNodes[directory].push(node);
+    });
+    return groupedNodes;
+}
+
+// Helper function which returns true, if the directory the node is in
+// contains both files (isLeaf) and subdirectories (isInternal).
+function hasFilesAndDirs(node: NodeApi<TreeNode>): boolean {
+    const parent = node.parent;
+    const children = parent?.children;
+    const nFiles = children?.filter((node) => node.isLeaf).length;
+    const nDirs = children?.filter((node) => node.isInternal).length;
+    if (nFiles && nFiles > 0 && nDirs && nDirs > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Helper function to check if path1 is a subset of path2
+function isPathSubset(path1: string, path2: string): boolean {
+    // Remove "/**" from the paths before splitting them into parts
+    const parts1 = path1.replace("/**", "").split("/");
+    const parts2 = path2.replace("/**", "").split("/");
+    return parts1.every((part, index) => part === parts2[index]);
+}
+
+// Helper function to remove lower-level paths from a string[] of paths
+// Example: if the input is ["a/b/**", "a/**"], the output will be ["a/**"]
+function removeInnerSubstitutions(paths: string[]): string[] {
+    return paths.filter(
+        (path, _, self) =>
+            !self.some(
+                (otherPath) =>
+                    otherPath !== path && isPathSubset(otherPath, path),
+            ),
+    );
+}
+
+// Helper function that takes an array of file paths and groups them
+// into a single glob pattern
+function generateGlobPattern(paths: string[]): string {
+    const patterns: string[] = [];
+
+    // Group paths by directory
+    const groupedPaths: { [key: string]: string[] } = {};
+    paths.forEach((path) => {
+        const directory =
+            path.lastIndexOf("/") === -1
+                ? ""
+                : path.substring(0, path.lastIndexOf("/") + 1);
+        const filename = path.substring(directory.length);
+        if (!groupedPaths[directory]) {
+            groupedPaths[directory] = [];
+        }
+        groupedPaths[directory].push(filename);
+    });
+
+    // Construct glob pattern for each directory
+    for (const directory in groupedPaths) {
+        const files = groupedPaths[directory];
+        let pattern;
+        // Handle root directory separately
+        if (directory === "") {
+            pattern = files.length === 1 ? files[0] : `{${files.join(",")}}`;
+        } else {
+            // Construct pattern based on the number of selected files
+            pattern =
+                files.length === 1
+                    ? `${directory}${files[0]}`
+                    : `${directory}{${files.join(",")}}`;
+        }
+        patterns.push(pattern);
+    }
+
+    // Join the patterns with ',' to create a single glob pattern
+    let globPattern = patterns.join(",");
+
+    // Surround the pattern with curly braces if it contains multiple patterns
+    if (patterns.length > 1) {
+        globPattern = `{${globPattern}}`;
+    }
+
+    return globPattern;
+}
+
+// Helper function that takes an internal node (directory) and returns
+// all its descendants that have a selectionStatus of 1 (selected)
+function getDescendants(node: NodeApi<TreeNode>): NodeApi<TreeNode>[] {
+    const descendants: NodeApi<TreeNode>[] = [];
+    if (node.children) {
+        node.children.forEach((child) => {
+            if (child.data.selectionStatus === 1) {
+                descendants.push(child);
+            } else {
+                descendants.push(...getDescendants(child));
+            }
+        });
+    }
+    return descendants;
+}
+
+// Helper function that takes an internal node (directory) and if its selectionStatus
+// is 1 (selected), removes it and all its descendants from the selectedNodes list
+function removeNodeAndDescendants(
+    node: NodeApi<TreeNode>,
+    nodes: NodeApi<TreeNode>[],
+) {
+    if (node.data.selectionStatus === 1) {
+        nodes = nodes.filter((n) => {
+            return n !== node && !getDescendants(node).includes(n);
+        });
+    }
+    return nodes;
+}
