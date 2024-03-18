4e702b564eab4e544572dc592bf18f2cac7274b3
Etsija <jyrki.keisala@gmail.com>
2024-03-18 09:32:55 +0200

    feat(ui): Use different schemas for BC and PE patterns
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/schemes/pattern_schema.ts
+++ apps/clearance_ui/src/schemes/pattern_schema.ts
@@ -5,7 +5,29 @@
 import isGlob from "is-glob";
 import { z } from "zod";
 
-export const patternGlobSchema = z
+export const bcPatternGlobSchema = z
+    .string()
+    .min(1, "Pattern cannot be empty")
+    .refine(
+        (pattern) => {
+            // Check if the pattern is a valid glob or matches a single file in a filepath:
+            //   ([.\w-]+\/)* matches zero or more occurrences of a ".", "-", "\w" characters (directory) followed by a slash
+            //   [.\w-]+ matches the file name with ".", "-", or "\w" allowed
+            //   (\.\w+)? makes the file extension part optional
+            return (
+                isGlob(pattern) || /^([.\w-]+\/)*[.\w-]+(\.\w+)?$/.test(pattern)
+            );
+        },
+        {
+            message:
+                "Pattern is not a valid glob pattern or a single file path",
+        },
+    )
+    .refine((pattern) => pattern !== "**", {
+        message: "You cannot do a bulk conclusion for all files in a package",
+    });
+
+export const pePatternGlobSchema = z
     .string()
     .min(1, "Pattern cannot be empty")
     .refine(
