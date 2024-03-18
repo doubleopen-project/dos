3a6e95ef85591cdb7302bea3e901e819a5fcf15f
Etsija <jyrki.keisala@gmail.com>
2024-03-18 13:07:02 +0200

    fix(ui): Tune path exclusion glob validation pattern
    Add a new rule for validating a glob pattern with curly braces. Also
    add an informative message saying why validation fails if no path is
    prepending the curly braces.
    
    Signed-off-by: Etsija <jyrki.keisala@gmail.com>

--- apps/clearance_ui/src/schemes/pattern_schema.ts
+++ apps/clearance_ui/src/schemes/pattern_schema.ts
@@ -4,6 +4,7 @@
 
 import isGlob from "is-glob";
 import { z } from "zod";
+import { validateCurlyBracesInGlob } from "@/helpers/validateCurlyBracesInGlob";
 
 export const bcPatternGlobSchema = z
     .string()
@@ -32,7 +33,7 @@ export const pePatternGlobSchema = z
     .min(1, "Pattern cannot be empty")
     .refine(
         (pattern) => {
-            // Check if the pattern is a valid glob or matches a single file in a filepath:
+            // Check that the pattern is a valid glob or matches a single file in a filepath:
             //   ([.\w-]+\/)* matches zero or more occurrences of a ".", "-", "\w" characters (directory) followed by a slash
             //   [.\w-]+ matches the file name with ".", "-", or "\w" allowed
             //   (\.\w+)? makes the file extension part optional
@@ -45,6 +46,17 @@ export const pePatternGlobSchema = z
                 "Pattern is not a valid glob pattern or a single file path",
         },
     )
+    .refine(
+        (pattern) => {
+            // Check that whenever using curly braces in a glob pattern for path exclusion,
+            // it always has a prepending non-empty path.
+            return validateCurlyBracesInGlob(pattern);
+        },
+        {
+            message:
+                "Due to ORT limitations, this pattern is invalid. Possible reasons: 1) You cannot exclude more than one file from root directory at once; 2) You cannot exclude files from several directories at once",
+        },
+    )
     .refine((pattern) => pattern !== "**", {
-        message: "You cannot do a bulk conclusion for all files in a package",
+        message: "You cannot do a path exclusion for all files in a package",
     });
