94e01aa5f4df72858e902b1ae0be1228bd8c8f03
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-03-26 13:02:18 +0200

    refactor(ui): Edit MultiSection component styling
    The forms will have more fields, so the forms will look better when the whole page can be scrolled,
    and not just the form itself.
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- apps/clearance_ui/src/components/common/MultiSection.tsx
+++ apps/clearance_ui/src/components/common/MultiSection.tsx
@@ -29,10 +29,10 @@ const MultiSection = ({
 
     return (
         <div className={cn("p-2 md:h-full", className)}>
-            <div className="m-1 h-full w-full rounded-md border p-10 shadow-lg sm:p-20">
+            <div className="m-1 w-full p-10 sm:p-20">
                 <h1 className="pb-2">{title}</h1>
                 <Separator />
-                <div className="flex flex-col space-y-8 pt-4 md:h-full lg:flex-row lg:space-x-12 lg:space-y-0">
+                <div className="flex flex-col space-y-8 pt-4 lg:flex-row lg:space-x-12 lg:space-y-0">
                     <aside className="lg:w-1/5">
                         <MultiSectionNavBar
                             defaultSection={defaultSection}
@@ -43,12 +43,12 @@ const MultiSection = ({
                             }))}
                         />
                     </aside>
-                    <div className="flex-1 overflow-hidden lg:max-w-2xl">
+                    <div className="flex-1 lg:max-w-2xl">
                         {sections.map((section) => (
                             <div
                                 key={section.title}
                                 className={cn(
-                                    "h-5/6 w-full overflow-auto rounded-md border p-10 shadow-lg sm:h-5/6 md:h-full lg:h-full lg:p-20",
+                                    "w-full rounded-md border p-10 shadow-lg lg:p-20",
                                     searchParamSection === section.tag
                                         ? "visible"
                                         : "hidden",
