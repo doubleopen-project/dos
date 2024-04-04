23c390bb200709f3787f6b9f4d5b03254fb7cef9
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2024-04-04 15:13:08 +0300

    refactor(ui): Add edit button on BC table in Clearance Library
    Only renders the button, no functionality yet
    
    Signed-off-by: Johanna Lamppu <johanna.e.lamppu@gmail.com>

--- apps/clearance_ui/src/components/clearance_library/bulk_conclusions/ActionCell.tsx
+++ apps/clearance_ui/src/components/clearance_library/bulk_conclusions/ActionCell.tsx
@@ -7,6 +7,7 @@ import { ZodiosResponseByAlias } from "@zodios/core";
 import { userAPI } from "validation-helpers";
 import { useUser } from "@/hooks/useUser";
 import DeleteBulkConclusion from "@/components/common/delete_item/DeleteBulkConclusion";
+import EditButton from "@/components/common/edit_item/EditButton";
 
 type BulkConclusion = ZodiosResponseByAlias<
     typeof userAPI,
@@ -20,7 +21,14 @@ const ActionCell = ({ row }: CellContext<BulkConclusion, unknown>) => {
         <>
             {(user.role === "ADMIN" ||
                 user.username === row.original.user.username) && (
-                <DeleteBulkConclusion id={row.original.id} />
+                <div className="flex items-center">
+                    <EditButton
+                        onClick={() => {}}
+                        name="edit"
+                        className="mr-1 px-2"
+                    />
+                    <DeleteBulkConclusion id={row.original.id} />
+                </div>
             )}
         </>
     ) : null;
