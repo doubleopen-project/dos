176a21b21a58254f12d346761e6f747148b271d0
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-04-11 12:52:22 +0300

    feat(ui): Save edited PE data to database
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- apps/clearance_ui/src/components/clearance_library/path_exclusions/ActionCell.tsx
+++ apps/clearance_ui/src/components/clearance_library/path_exclusions/ActionCell.tsx
@@ -2,12 +2,17 @@
 //
 // SPDX-License-Identifier: MIT
 
+import { useQueryClient } from "@tanstack/react-query";
 import { CellContext } from "@tanstack/react-table";
-import { ZodiosResponseByAlias } from "@zodios/core";
-import { Check, X } from "lucide-react";
+import { ZodiosBodyByAlias, ZodiosResponseByAlias } from "@zodios/core";
+import { isAxiosError } from "axios";
+import { Check, Loader2, X } from "lucide-react";
+import { useSession } from "next-auth/react";
 import { userAPI } from "validation-helpers";
 import { useUser } from "@/hooks/useUser";
+import { userHooks } from "@/hooks/zodiosHooks";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
 import DeletePathExclusion from "@/components/common/delete_item/DeletePathExclusion";
 import EditButton from "@/components/common/edit_item/EditButton";
 
@@ -16,10 +21,58 @@ type PathExclusion = ZodiosResponseByAlias<
     "GetPathExclusions"
 >["pathExclusions"][0];
 
+type PathExclusionUpdateData = ZodiosBodyByAlias<
+    typeof userAPI,
+    "PutPathExclusion"
+>;
+
 const ActionCell = ({ row, table }: CellContext<PathExclusion, unknown>) => {
     // Get user role, to decide what rights the user has for this view
     const user = useUser();
+    const session = useSession();
+    const peKey = userHooks.getKeyByAlias("GetPathExclusions");
+    const queryClient = useQueryClient();
     const meta = table.options.meta;
+
+    const { mutate: UpdatePathExclusion, isLoading } =
+        userHooks.usePutPathExclusion(
+            {
+                headers: {
+                    Authorization: `Bearer ${session.data?.accessToken}`,
+                },
+                params: {
+                    id: row.original.id,
+                },
+            },
+            {
+                onSuccess: () => {
+                    meta?.setSelectedRowsForEditing(() => {
+                        const old = meta.selectedRowsForEditing;
+                        return {
+                            ...old,
+                            [parseInt(row.id)]: false,
+                        };
+                    });
+                    meta?.updateOriginalData(row.index);
+                    queryClient.invalidateQueries(peKey);
+                    toast({
+                        title: "Path exclusion updated",
+                        description: "Path exclusion updated successfully",
+                    });
+                },
+                onError: (error) => {
+                    const msg = isAxiosError(error)
+                        ? error.response?.data.message
+                        : error.message;
+
+                    toast({
+                        variant: "destructive",
+                        title: "Error",
+                        description: msg,
+                    });
+                },
+            },
+        );
 
     const handleCancel = () => {
         meta?.revertData(row.index);
@@ -32,7 +85,14 @@ const ActionCell = ({ row, table }: CellContext<PathExclusion, unknown>) => {
         });
     };
 
-    const handleSave = () => {};
+    const handleSave = () => {
+        const body: PathExclusionUpdateData = {
+            pattern: row.getValue("pattern"),
+            reason: row.getValue("reason"),
+            comment: row.getValue("comment"),
+        };
+        UpdatePathExclusion(body);
+    };
 
     const handleEdit = () => {
         meta?.setSelectedRowsForEditing(() => {
@@ -52,7 +112,11 @@ const ActionCell = ({ row, table }: CellContext<PathExclusion, unknown>) => {
                 variant="outline"
                 className="mr-1 px-2"
             >
-                <Check size={16} className="text-green-400" />
+                {isLoading ? (
+                    <Loader2 size={16} className="animate-spin" />
+                ) : (
+                    <Check size={16} className="text-green-400" />
+                )}
             </Button>
             <Button
                 onClick={handleCancel}
