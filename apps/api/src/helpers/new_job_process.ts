109a5d9a7a93ccf776a9a5d7b43a896521a66c01
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-04-19 18:01:18 +0300

    refactor(api): Add debug logging for new job process
    With this simple logger, more information of the new job process can be gained
    with setting the environment variable LOG_LEVEL to 'debug'.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- apps/api/src/helpers/new_job_process.ts
+++ apps/api/src/helpers/new_job_process.ts
@@ -2,12 +2,18 @@
 //
 // SPDX-License-Identifier: MIT
 
+import log from "loglevel";
 import { saveFilesWithHashKey } from "s3-helpers";
 import { findFilesToBeScanned } from "./db_operations";
 import * as dbQueries from "./db_queries";
 import * as fileHelpers from "./file_helpers";
 import { s3Client } from "./s3client";
 import { sendJobToQueue } from "./sa_queries";
+
+const logLevel: log.LogLevelDesc =
+    (process.env.LOG_LEVEL as log.LogLevelDesc) || "info"; // trace/debug/info/warn/error/silent
+
+log.setLevel(logLevel);
 
 export const processPackageAndSendToScanner = async (
     zipFileKey: string,
@@ -17,16 +23,19 @@ export const processPackageAndSendToScanner = async (
     jobStateMap: Map<string, string>,
 ) => {
     try {
-        console.log(
+        log.info(
             scannerJobId + ": Processing files for purl(s) " + purls.join(", "),
         );
 
+        log.debug(scannerJobId + ": Search for main Scanner Job");
         const mainScannerJob = await dbQueries.findScannerJobById(scannerJobId);
 
         if (!mainScannerJob) throw new Error("ScannerJob not found");
 
+        // Create ScannerJobs for each package
         const scannerJobIds: string[] = [scannerJobId];
         if (packageIds.length > 1) {
+            log.debug(scannerJobId + ": Creating ScannerJobs for each package");
             for (const packageId of packageIds) {
                 if (packageId !== mainScannerJob.packageId) {
                     const createdScannerJob = await dbQueries.createScannerJob({
@@ -39,70 +48,102 @@ export const processPackageAndSendToScanner = async (
             }
         }
         // Update Package scanStatuses to 'pending'
+        log.debug(
+            scannerJobId + ": Updating Package scanStatuses to 'pending'",
+        );
         await dbQueries.updateManyPackagesScanStatuses(packageIds, "pending");
         // Update ScannerJob statuses to 'processing'
+        log.debug(
+            scannerJobId + ": Updating ScannerJob statuses to 'processing'",
+        );
         await dbQueries.updateManyScannerJobStates(scannerJobIds, "processing");
         // Downloading zip file from object storage
         const downloadPath = "/tmp/downloads/" + zipFileKey;
+        log.info(scannerJobId + ": Downloading zip file from object storage");
         const downloaded = await fileHelpers.downloadZipFile(
             zipFileKey,
             downloadPath,
         );
 
         if (!downloaded) {
+            log.debug(
+                scannerJobId +
+                    ': Changing Package(s) scanStatus(es) to "failed"',
+            );
             dbQueries.updateManyPackagesScanStatuses(packageIds, "failed");
+            log.debug(
+                scannerJobId + ': Changing ScannerJob(s) state(s) to "failed"',
+            );
             dbQueries.updateManyScannerJobStates(scannerJobIds, "failed");
             throw new Error("Zip file download failed");
         }
 
-        //console.log(scannerJobId + ': Zip file downloaded');
+        log.debug(scannerJobId + ": Zip file downloaded");
 
         // Unzipping the file locally
         const fileNameNoExt = zipFileKey.split(".")[0];
         const basePath = "/tmp/extracted/";
         const extractPath = basePath + fileNameNoExt + "/";
 
+        log.debug(scannerJobId + ": Unzipping the file locally");
         const fileUnzipped = await fileHelpers.unzipFile(
             downloadPath,
             extractPath,
         );
 
         if (!fileUnzipped) {
+            log.debug(
+                scannerJobId +
+                    ': Changing Package(s) scanStatus(es) to "failed"',
+            );
             dbQueries.updateManyPackagesScanStatuses(packageIds, "failed");
+            log.debug(
+                scannerJobId + ': Changing ScannerJob(s) state(s) to "failed"',
+            );
             dbQueries.updateManyScannerJobStates(scannerJobIds, "failed");
             throw new Error("Zip file unzipping failed");
         }
 
-        //console.log(scannerJobId + ': Zip file unzipped');
+        log.debug(scannerJobId + ": Zip file unzipped");
 
         // Saving files in extracted folder to object storage
 
         // Mapping file hashes and the corresponding paths in an array
+        log.debug(scannerJobId + ": Mapping file hashes and paths");
         const fileHashesMappedToPaths: {
             filesCount: number;
             fileHashesAndPaths: Map<string, string[]>;
         } = await fileHelpers.getFileHashesMappedToPaths(extractPath);
 
-        console.log(
+        log.info(
             scannerJobId +
                 ": Found files: " +
                 fileHashesMappedToPaths.filesCount,
         );
-        //console.log(scannerJobId + ': Found unique files: ' + fileHashesMappedToPaths.fileHashesAndPaths.size);
+        log.debug(
+            scannerJobId +
+                ": Found unique files: " +
+                fileHashesMappedToPaths.fileHashesAndPaths.size,
+        );
 
         // Save FileTrees to existing Files (and for files with multiple paths) and get array of files to be scanned
+        log.debug(scannerJobId + ": Finding files to be scanned");
         let filesToBeScanned: { hash: string; path: string }[] | null =
             await findFilesToBeScanned(
                 packageIds,
                 fileHashesMappedToPaths.fileHashesAndPaths,
             );
-        console.log(
+        log.info(
             scannerJobId + ": Uploading and sending to be scanned: ",
             filesToBeScanned.length,
         );
 
         // Uploading files to object storage with the file hash as the key
         if (filesToBeScanned.length > 0) {
+            log.debug(
+                scannerJobId +
+                    ": Uploading files to object storage with the file hash as the key",
+            );
             const uploadedWithHash = await saveFilesWithHashKey(
                 s3Client,
                 process.env.SPACES_BUCKET || "doubleopen",
@@ -113,7 +154,18 @@ export const processPackageAndSendToScanner = async (
             );
 
             if (!uploadedWithHash) {
+                log.debug(
+                    scannerJobId + ": Uploading files to object storage failed",
+                );
+                log.debug(
+                    scannerJobId +
+                        ': Changing Package(s) scanStatus(es) to "failed"',
+                );
                 dbQueries.updateManyPackagesScanStatuses(packageIds, "failed");
+                log.debug(
+                    scannerJobId +
+                        ': Changing ScannerJob(s) state(s) to "failed"',
+                );
                 dbQueries.updateManyScannerJobStates(scannerJobIds, "failed");
                 throw new Error(
                     "Error: Uploading files to object storage failed",
@@ -122,14 +174,14 @@ export const processPackageAndSendToScanner = async (
         }
         jobStateMap.delete(scannerJobId);
 
-        //console.log('Files uploaded to object storage');
+        log.debug(scannerJobId + ": Files uploaded to object storage");
 
         // Deleting local files
         fileHelpers.deleteLocalFiles(downloadPath, extractPath);
-        //console.log('Local files deleted');
+        log.debug(scannerJobId + ": Local files deleted");
 
         if (filesToBeScanned.length > 0) {
-            console.log(
+            log.info(
                 scannerJobId +
                     ": Sending a request to Scanner Agent to add new job to the work queue",
             );
@@ -141,7 +193,7 @@ export const processPackageAndSendToScanner = async (
             );
 
             if (addedToQueue) {
-                console.log(
+                log.info(
                     scannerJobId + ': Updating ScannerJob state to "queued"',
                 );
 
@@ -159,24 +211,30 @@ export const processPackageAndSendToScanner = async (
                 throw new Error("Error: Adding to queue was unsuccessful.");
             }
         } else {
+            log.debug(scannerJobId + ": No files to be scanned");
+            log.debug(
+                scannerJobId + ': Changing Package scanStatus to "scanned"',
+            );
             dbQueries.updateManyPackagesScanStatuses(packageIds, "scanned");
+            log.debug(
+                scannerJobId + ': Changing ScannerJob state to "completed"',
+            );
             dbQueries.updateManyScannerJobStates(scannerJobIds, "completed");
         }
         fileHashesMappedToPaths.fileHashesAndPaths.clear();
         filesToBeScanned = null;
+        log.debug(scannerJobId + ": Processing phase completed");
     } catch (error) {
-        console.log(error);
+        log.error(error);
         try {
-            console.log(
-                scannerJobId + ': Changing ScannerJob state to "failed"',
-            );
+            log.info(scannerJobId + ': Changing ScannerJob state to "failed"');
             dbQueries.updateScannerJob(scannerJobId, { state: "failed" });
-            console.log(
+            log.info(
                 scannerJobId + ': Changing Package scanStatus to "failed"',
             );
             dbQueries.updateManyPackagesScanStatuses(packageIds, "failed");
         } catch (error) {
-            console.log(
+            log.error(
                 scannerJobId +
                     ': Unable to update ScannerJob and Package statuses to "failed"',
             );
