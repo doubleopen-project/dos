3310963e98b1ff26ecb2b1e2a18b4a854472c4bb
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-04-22 15:07:50 +0300

    refactor(database): Fix prisma client instantiation
    As per the Prisma documentation, the application should only create one instance
    of PrismaClient. This was the initial intended approach, but has been replaced
    due to unsolved TypeScript issues. These issues however have been resolved in
    this commit, and the prisma client can be properly imported from the database
    package now.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- packages/database/index.d.ts
+++ packages/database/index.d.ts
@@ -2,19 +2,49 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { PrismaClient } from "@prisma/client";
 import * as _prisma_client_runtime_library from "@prisma/client/runtime/library";
 import * as _prisma_client from ".prisma/client";
 
 export * from "@prisma/client";
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
-declare const prisma: PrismaClient<
-    _prisma_client.Prisma.PrismaClientOptions,
-    never,
-    _prisma_client_runtime_library.DefaultArgs
+declare const prisma: _prisma_client_runtime_library.DynamicClientExtensionThis<
+    _prisma_client.Prisma.TypeMap<
+        _prisma_client_runtime_library.InternalArgs & {
+            result: {
+                licenseFinding: {
+                    licenseExpressionSPDX: () => {
+                        needs: {
+                            unprocessedLicenseExpressionSPDX: true;
+                        };
+                        compute(licenseFinding: {
+                            unprocessedLicenseExpressionSPDX: string;
+                        }): string;
+                    };
+                };
+            };
+            model: {};
+            query: {};
+            client: {};
+        }
+    >,
+    _prisma_client.Prisma.TypeMapCb,
+    {
+        result: {
+            licenseFinding: {
+                licenseExpressionSPDX: () => {
+                    needs: {
+                        unprocessedLicenseExpressionSPDX: true;
+                    };
+                    compute(licenseFinding: {
+                        unprocessedLicenseExpressionSPDX: string;
+                    }): string;
+                };
+            };
+        };
+        model: {};
+        query: {};
+        client: {};
+    }
 >;
 
 export { prisma };
