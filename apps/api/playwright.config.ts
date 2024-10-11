9c8d57df5744e17eb12e4381a0010b30e7cd8d76
Mikko Murto <mikko.murto@doubleopen.org>
2024-10-11 10:26:13 +0300

    test: Add E2E API test to ensure Scanner Worker works
    Signed-off-by: Mikko Murto <mikko.murto@doubleopen.org>

---
+++ apps/api/playwright.config.ts
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: 2024 Double Open Oy
+//
+// SPDX-License-Identifier: MIT
+
+import { defineConfig } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+    /* Timeout for each test, to accommodate for slower machines in local testing */
+    timeout: 60000,
+
+    testDir: "./tests/e2e",
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? 1 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: "html",
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        baseURL: "http://localhost:3001",
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: "on-first-retry",
+
+        video: "off",
+    },
+
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: "API tests",
+            testMatch: "**/*.spec.ts",
+        },
+    ],
+});
