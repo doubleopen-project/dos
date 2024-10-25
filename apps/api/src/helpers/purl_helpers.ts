d66af8df5f21a1149cf4f9253af851f92df5842a
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2024-10-25 10:46:54 +0300

    fix(api): Edit parsing of qualifiers from purl
    As the qualifiers are saved as a string in the database and
    the individual key value pairs are not saved or processed
    separately, and their processing is causing inconvenient errors,
    parse the qualifier string straight from the purl as is.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- apps/api/src/helpers/purl_helpers.ts
+++ apps/api/src/helpers/purl_helpers.ts
@@ -7,23 +7,12 @@ import { PackageURL } from "packageurl-js";
 export const parsePurl = (purl: string) => {
     const parsedPurl = PackageURL.fromString(purl);
 
-    let qualifiers = undefined;
-
-    if (parsedPurl.qualifiers) {
-        for (const [key, value] of Object.entries(parsedPurl.qualifiers)) {
-            if (qualifiers) {
-                qualifiers += "&" + key + "=" + encodeURIComponent(value);
-            } else {
-                qualifiers = key + "=" + encodeURIComponent(value);
-            }
-        }
-    }
     return {
         type: parsedPurl.type,
         namespace: parsedPurl.namespace,
         name: parsedPurl.name,
         version: parsedPurl.version,
-        qualifiers: qualifiers,
+        qualifiers: purl.split("#")[0].split("?")[1],
         subpath: parsedPurl.subpath,
     };
 };
