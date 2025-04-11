ecea343e382b3de8feee9574a43e90652780d4dd
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2025-04-11 13:20:13 +0300

    chore: Dockerize Playwright tests and update Playwright workflow
    To help resolve issues with an upcoming change that adds a Keycloak Docker
    Compose setup for local development, and which will be used in the GitHub
    Playwright workflow as well, edit the setup so that the tests in the GitHub
    workflow will be run in Docker Compose. This way all the services will be
    on the same network, and there won't be a need for tricky workarounds or
    larger refactorings to allow the services to communicate with each other.
    
    With this change, also an earlier workaround in the API tests for fetching
    the presigned URL needed to be removed.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- apps/clearance_ui/playwright.config.ts
+++ apps/clearance_ui/playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: process.env.CI
-            ? "http://localhost:3002"
+            ? "http://clearance-ui:3000"
             : "http://localhost:3000",
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
