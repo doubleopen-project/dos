ef0ef4bd4332dfd668611c79a1383ae983c0d022
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2025-06-04 09:38:54 +0300

    refactor(api): Migrate to API tokens not tied to user accounts
    The API tokens that are used to authenticate access to getting scan results,
    adding jobs and getting package configurations, used to be personal tokens
    for all users. This is changed as there's a need for more restrictions to
    the access to these endpoints.
    
    As the number of needed tokens is low and it is expected that they rarely
    change, these tokens are added through an environment variable for
    simplicity and faster access. However, if the need emerges, `CRUD` support
    for token handling for server admins could be added later.
    
    The main motivation for this change comes from developing DOS alongside the
    ORT Server, and from an upcoming feature on DOS that will allow users to
    create their own clearance items. These clearance items will be linked to
    the organization the user belongs in, and for example the package
    configurations endpoint will support returning clearance items based on an
    organization ID, and so the need to restrict access to this endpoint is
    vital, as users shouldn't be allowed to access clearance items on other
    organizations than their own.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- apps/api/src/helpers/auth_helpers.ts
+++ apps/api/src/helpers/auth_helpers.ts
@@ -4,9 +4,13 @@
 
 import { NextFunction, Request, Response } from "express";
 import NodeCache from "node-cache";
-import { getUsers } from "./keycloak_queries";
 
 const cache = new NodeCache({ stdTTL: 5 * 60, checkperiod: 60 });
+
+const tokens =
+    process.env.API_TOKENS?.split(",")
+        .map((token) => token.trim())
+        .filter((token) => token.length > 0) || [];
 
 export const authenticateDosApiToken = async (
     req: Request,
@@ -14,22 +18,26 @@ export const authenticateDosApiToken = async (
     next: NextFunction,
 ) => {
     try {
-        const authHeader = req.headers["authorization"];
-        const token = authHeader && authHeader.split(" ")[1];
-
-        if (token == null)
-            return res.status(401).json({ message: "Unauthorized" });
-
-        // Cache is used for (GET) job-state requests
-        const useCache = req.originalUrl.split("/")[2] === "job-state";
-
-        if (useCache && cache.has(token)) next();
+        // Allow access to the API in development without setting tokens.
+        if (process.env.NODE_ENV !== "production" && tokens.length === 0)
+            next();
         else {
-            const users = await getUsers(undefined, token, undefined);
-            if (users.length > 0) {
-                cache.set(token, true);
-                next();
-            } else return res.status(403).json({ message: "Forbidden" });
+            const authHeader = req.headers["authorization"];
+            const token = authHeader && authHeader.split(" ")[1];
+
+            if (token == null)
+                return res.status(401).json({ message: "Unauthorized" });
+
+            // Cache is used for (GET) job-state requests
+            const useCache = req.originalUrl.split("/")[2] === "job-state";
+
+            if (useCache && cache.has(token)) next();
+            else {
+                if (tokens.includes(token)) {
+                    cache.set(token, true);
+                    next();
+                } else return res.status(403).json({ message: "Forbidden" });
+            }
         }
     } catch (error) {
         console.log(error);
