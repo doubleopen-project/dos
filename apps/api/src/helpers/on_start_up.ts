1ed0a051af9e0cbd22fe12a0d6375c54d2650d8e
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2025-11-07 12:23:49 +0200

    fix(api): Fix logic for handling the saving results state
    Previously the assumption was that if a job was found by the `jobStateQuery`
    cron job in the `savingResults` state and stayed in that state for longer
    than 30 minutes, that the server had likely restarted, and so the saving of
    the results was restarted in that case. However, in the case where the
    actual saving of the results takes a lot longer, the saving of the results
    was actually restarted multiple times until the server got overloaded with
    simultaneously handling the large results and crashed due to a "JavaScript
    heap out of memory" error.
    
    Change this so that in the case of a server restart, the saving of the
    results is restarted in the `onStartUp` function, but only if it hasn't
    been restarted before. If there has already been a restart, set the status
    to failed, as this indicates that the saving of the results may have
    crashed the server. In the `jobStateQuery`, set a threshold for how long
    a job can be in the `savingResults` state until it is marked as failed.
    Leave the job in the queue for inspection when setting the state to failed.
    A cron job for cleaning old jobs from the queue will be added in a
    subsequent commit.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- apps/api/src/helpers/on_start_up.ts
+++ apps/api/src/helpers/on_start_up.ts
@@ -3,14 +3,19 @@
 // SPDX-License-Identifier: MIT
 
 import log from "loglevel";
+import QueueService from "../services/queue";
+import { saveJobResults } from "./db_operations";
 import {
     createSystemIssue,
     findScannerJobsByState,
     findSystemIssues,
     updateManyPackagesScanStatuses,
     updateManyScannerJobStates,
+    updateScannerJobAndPackagesStateToFailedRecursive,
     updateSystemIssue,
 } from "./db_queries";
+
+const workQueue = QueueService.getInstance();
 
 const logLevel: log.LogLevelDesc =
     (process.env.LOG_LEVEL as log.LogLevelDesc) || "info"; // trace/debug/info/warn/error/silent
@@ -20,6 +25,7 @@ log.setLevel(logLevel);
 export const onStartUp = async () => {
     try {
         await checkJobsInProcessingState();
+        await checkJobsInSavingResultsState();
 
         log.info(
             "All onStartUp checks completed successfully. Moving on to start the server.",
@@ -98,6 +104,89 @@ const checkJobsInProcessingState = async () => {
                         "Severity updated to CRITICAL as the same issue was found multiple times, which implies that the processing of this package is causing the server to restart",
                 });
             }
+        }
+    }
+};
+
+const checkJobsInSavingResultsState = async () => {
+    log.info("Checking for jobs in saving results state...");
+
+    const jobs = await findScannerJobsByState("savingResults", true);
+
+    log.info("Found " + jobs.length + " jobs in saving results state");
+
+    if (jobs.length === 0) {
+        log.info("No jobs in saving results state.");
+        return;
+    }
+
+    const errorCode = "SAVING_RESULTS_INTERRUPTED";
+    const severity = "LOW";
+    const msgBase =
+        "Saving results for the job was interrupted by server restart for package with purl ";
+
+    for (const job of jobs) {
+        log.debug(
+            "Finding out if the job " + job.id + " has a SystemIssue already",
+        );
+        const existingIssues = await findSystemIssues(
+            errorCode,
+            msgBase + job.package.purl,
+            false,
+        );
+
+        const workQueueJob = await workQueue.getJob(job.id);
+
+        if (existingIssues.length === 0) {
+            log.info("Filing a SystemIssue for job " + job.id);
+            await createSystemIssue({
+                message: msgBase + job.package.purl,
+                severity: severity,
+                errorCode: errorCode,
+                info: {
+                    latestJobId: [job.id],
+                    packageId: job.package.id,
+                },
+            });
+
+            const result = workQueueJob?.returnvalue.result;
+
+            if (workQueueJob && result) {
+                log.info(job.id + ": Restarting saving results phase.");
+
+                saveJobResults(job.id, JSON.parse(result), undefined);
+            } else {
+                log.error(
+                    job.id +
+                        ": Could not restart saving results as work queue job or result is missing.",
+                );
+
+                log.info("Setting the state of the job to 'failed'");
+                await updateScannerJobAndPackagesStateToFailedRecursive(
+                    job.id,
+                    "Saving results was interrupted by server restart, and could not be automatically restarted.",
+                );
+            }
+        } else {
+            log.info("SystemIssue(s) found for package " + job.package.purl);
+            for (const issue of existingIssues) {
+                log.info("Updating SystemIssue");
+                await updateSystemIssue(issue.id, {
+                    info: {
+                        latestJobId: [job.id],
+                        packageId: job.package.id,
+                    },
+                    severity: "CRITICAL",
+                    details:
+                        "Severity updated to CRITICAL as the same issue was found multiple times, which implies that saving results for this package is causing the server to crash.",
+                });
+            }
+
+            log.info("Setting the state of the job to 'failed'");
+            await updateScannerJobAndPackagesStateToFailedRecursive(
+                job.id,
+                "Saving results failed, due to server crash or restart.",
+            );
         }
     }
 };
