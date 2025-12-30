4c001c40e6d5c46359c47bba0a443a47c40df20e
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2025-12-30 17:05:09 +0200

    feat(ui): Add no groups handling in `ClearanceActionNotice`
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- apps/clearance_ui/src/hooks/useClearanceActionState/index.ts
+++ apps/clearance_ui/src/hooks/useClearanceActionState/index.ts
@@ -3,12 +3,14 @@
 // SPDX-License-Identifier: MIT
 
 import { useUser } from "@/hooks/useUser";
+import useContextStore from "@/store/context.store";
 import { hasPermission } from "@/helpers/hasPermission";
 
-type ClearanceActionBlocker = "NO_PERMISSION" | null;
+type ClearanceActionBlocker = "NO_PERMISSION" | "NO_GROUP" | null;
 
 export const useClearanceActionState = () => {
     const user = useUser();
+    const selectedGroup = useContextStore((s) => s.selectedClearanceGroup);
 
     let blocker: ClearanceActionBlocker = null;
 
@@ -18,6 +20,8 @@ export const useClearanceActionState = () => {
         !hasPermission(user.permissions, "ClearanceItems", "POST")
     ) {
         blocker = "NO_PERMISSION";
+    } else if (selectedGroup === undefined) {
+        blocker = "NO_GROUP";
     }
 
     return {
