e5891a4c3d2b7355112f605410aa5af521ec4158
Johanna Lamppu <johanna.lamppu@doubleopen.org>
2026-01-28 11:12:39 +0200

    chore(api): Make Express request augmentation explicitly global
    Ensure Express Request type augmentation remains global even when the file
    contains imports, by explicitly using `declare global`. Without imports
    TypeScript treats the file as a global script, but when imports are added,
    it treats it as a module instead. This is done in preparation of extending
    the type in a way that requires an import.
    
    Signed-off-by: Johanna Lamppu <johanna.lamppu@doubleopen.org>

--- apps/api/src/@types/express.d.ts
+++ apps/api/src/@types/express.d.ts
@@ -2,55 +2,59 @@
 //
 // SPDX-License-Identifier: MIT
 
-declare namespace Express {
-    export interface Request {
-        hasRequiredRoles: boolean;
-        kauth: {
-            grant: {
-                access_token: {
-                    token: string;
-                    clientId: string;
-                    header: {
-                        alg: string;
-                        typ: string;
-                        kid: string;
-                    };
-                    content: {
-                        exp: number;
-                        iat: number;
-                        jti: string;
-                        iss: string;
-                        aud: string;
-                        sub: string;
-                        typ: string;
-                        azp: string;
-                        session_state: string;
-                        acr: string;
-                        "allowed-origins": string[];
-                        realm_access: {
-                            roles: string[];
+declare global {
+    namespace Express {
+        export interface Request {
+            hasRequiredRoles: boolean;
+            kauth: {
+                grant: {
+                    access_token: {
+                        token: string;
+                        clientId: string;
+                        header: {
+                            alg: string;
+                            typ: string;
+                            kid: string;
                         };
-                        resource_access: {
-                            [process.env.KEYCLOAK_CLIENT_ID_API ||
-                                "dos-dev-api"]: {
+                        content: {
+                            exp: number;
+                            iat: number;
+                            jti: string;
+                            iss: string;
+                            aud: string;
+                            sub: string;
+                            typ: string;
+                            azp: string;
+                            session_state: string;
+                            acr: string;
+                            "allowed-origins": string[];
+                            realm_access: {
                                 roles: string[];
                             };
-                            account: {
-                                roles: string[];
+                            resource_access: {
+                                [process.env.KEYCLOAK_CLIENT_ID_API ||
+                                    "dos-dev-api"]: {
+                                    roles: string[];
+                                };
+                                account: {
+                                    roles: string[];
+                                };
                             };
+                            scope: string;
+                            sid: string;
+                            email_verified: boolean;
+                            preferred_username: string;
+                            email: string;
+                            given_name: string;
+                            family_name: string;
                         };
-                        scope: string;
-                        sid: string;
-                        email_verified: boolean;
-                        preferred_username: string;
-                        email: string;
-                        given_name: string;
-                        family_name: string;
+                        signature: Buffer | unknown;
+                        signed: string;
                     };
-                    signature: Buffer | unknown;
-                    signed: string;
                 };
             };
-        };
+        }
     }
 }
+
+export {};
